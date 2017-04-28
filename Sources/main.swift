5adeb9223e1cabb6c3ea05009ddcd6c21339f2ea
Norman Maurer <norman_maurer@apple.com>
2017-04-28 15:58:39 +0200

    Remove enclosing do {...} catch {...} block

--- Sources/main.swift
+++ Sources/main.swift
@@ -57,86 +57,82 @@ defer {
     do { try server.close() } catch { }
 }
 
-do {
-    try server.setOption(level: SOL_SOCKET, name: SO_REUSEADDR, value: 1)
-    
-    while true {
-        // Block until there are events to handle
-        if let events = try selector.awaitReady() {
-            for ev in events {
-                if ev.isReadable {
-                    
-                    // We can handle either read(...) or accept()
-                    if ev.selectable is Socket {
-                        // We stored the Buffer before as attachment so get it and clear the limit / offset.
-                        let buffer = ev.attachment as! Buffer
-                        buffer.clear()
+try server.setOption(level: SOL_SOCKET, name: SO_REUSEADDR, value: 1)
 
-                        let s = ev.selectable as! Socket
-                        do {
-                            let read = try s.read(data: &buffer.data)
-                            if (read < 0) {
-                                continue
-                            }
-                            buffer.limit = read
+while true {
+    // Block until there are events to handle
+    if let events = try selector.awaitReady() {
+        for ev in events {
+            if ev.isReadable {
+                
+                // We can handle either read(...) or accept()
+                if ev.selectable is Socket {
+                    // We stored the Buffer before as attachment so get it and clear the limit / offset.
+                    let buffer = ev.attachment as! Buffer
+                    buffer.clear()
+                    
+                    let s = ev.selectable as! Socket
+                    do {
+                        let read = try s.read(data: &buffer.data)
+                        if (read < 0) {
+                            continue
+                        }
+                        buffer.limit = read
+                        
+                        let written = try s.write(data: buffer.data, offset: buffer.offset, len: buffer.limit - buffer.offset)
+                        if written >= 0 {
+                            buffer.offset += written
                             
-                            let written = try s.write(data: buffer.data, offset: buffer.offset, len: buffer.limit - buffer.offset)
-                            if written >= 0 {
-                                buffer.offset += written
-                                
-                                // We could not write everything so we reregister with InterestedEvent.Write and so get woken up once the socket becomes writable again.
-                                // This also ensure we not read anymore until we were able to echo it back (backpressure FTW).
-                                if buffer.offset < buffer.limit {
-                                    try selector.reregister(selectable: s, interested: InterestedEvent.Write)
-                                }
-                            } else {
-                                // We could not write everything so we reregister with InterestedEvent.Write and so get woken up once the socket becomes writable again.
-                                // This also ensure we not read anymore until we were able to echo it back (backpressure FTW).
+                            // We could not write everything so we reregister with InterestedEvent.Write and so get woken up once the socket becomes writable again.
+                            // This also ensure we not read anymore until we were able to echo it back (backpressure FTW).
+                            if buffer.offset < buffer.limit {
                                 try selector.reregister(selectable: s, interested: InterestedEvent.Write)
                             }
-                        } catch let err as IOError {
-                            deregisterAndClose(selector: selector, s: s)
+                        } else {
+                            // We could not write everything so we reregister with InterestedEvent.Write and so get woken up once the socket becomes writable again.
+                            // This also ensure we not read anymore until we were able to echo it back (backpressure FTW).
+                            try selector.reregister(selectable: s, interested: InterestedEvent.Write)
                         }
-                    } else if ev.selectable is ServerSocket {
-                        let socket = ev.selectable as! ServerSocket
-                        
-                        // Accept new connections until there are no more in the backlog
-                        while let accepted = try socket.accept() {
-                            try accepted.setNonBlocking()
-                            try accepted.setOption(level: SOL_SOCKET, name: SO_REUSEADDR, value: 1)
-
-                            
-                            // Allocate an 8kb buffer for reading and writing and register the socket with the selector
-                            let buffer = Buffer(capacity: 8 * 1024)
-                            try selector.register(selectable: accepted, attachment: buffer)
-                        }
+                    } catch let err as IOError {
+                        deregisterAndClose(selector: selector, s: s)
                     }
+                } else if ev.selectable is ServerSocket {
+                    let socket = ev.selectable as! ServerSocket
                     
-                } else if ev.isWritable {
-                    if ev.selectable is Socket {
-                        let buffer = ev.attachment as! Buffer
+                    // Accept new connections until there are no more in the backlog
+                    while let accepted = try socket.accept() {
+                        try accepted.setNonBlocking()
+                        try accepted.setOption(level: SOL_SOCKET, name: SO_REUSEADDR, value: 1)
                         
-                        let s = ev.selectable as! Socket
-                        do {
-                            let written = try s.write(data: buffer.data, offset: buffer.offset, len: buffer.limit - buffer.offset)
-                            if (written > 0) {
-                                buffer.offset += written
-                                
-                                if buffer.offset == buffer.limit {
-                                    // Everything was written, reregister again with InterestedEvent.Read so we are notified once there is more data on the socket to read.
-                                    try selector.reregister(selectable: s, interested: InterestedEvent.Read)
-                                }
+                        
+                        // Allocate an 8kb buffer for reading and writing and register the socket with the selector
+                        let buffer = Buffer(capacity: 8 * 1024)
+                        try selector.register(selectable: accepted, attachment: buffer)
+                    }
+                }
+                
+            } else if ev.isWritable {
+                if ev.selectable is Socket {
+                    let buffer = ev.attachment as! Buffer
+                    
+                    let s = ev.selectable as! Socket
+                    do {
+                        let written = try s.write(data: buffer.data, offset: buffer.offset, len: buffer.limit - buffer.offset)
+                        if (written > 0) {
+                            buffer.offset += written
+                            
+                            if buffer.offset == buffer.limit {
+                                // Everything was written, reregister again with InterestedEvent.Read so we are notified once there is more data on the socket to read.
+                                try selector.reregister(selectable: s, interested: InterestedEvent.Read)
                             }
-                        } catch let err as IOError {
-                            deregisterAndClose(selector: selector, s: s)
                         }
+                    } catch let err as IOError {
+                        deregisterAndClose(selector: selector, s: s)
                     }
                 }
             }
         }
     }
-} catch let err as IOError {
-    print("{}: {}", err.reason!, err.errno)
 }
 
 
