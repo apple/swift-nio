56c1478dfb1ed927f6e95387f54f14260da9ea80
Norman Maurer <norman_maurer@apple.com>
2017-12-05 14:54:41 +0100

    Add docs for SocketAddresses

--- Sources/NIO/SocketAddresses.swift
+++ Sources/NIO/SocketAddresses.swift
@@ -18,17 +18,23 @@
     import Glibc
 #endif
 
+/// Special `Error` that may be thrown if we fail to create a `SocketAddress`.
 public enum SocketAddressError: Error {
-    case unknown
+    /// The host is unknown (could not be resolved).
+    case unknown(host: String, port: Int32)
+    /// The requested `SocketAddress` is not supported.
     case unsupported
+    /// The requested UDS path is too long.
     case unixDomainSocketPathTooLong
 }
 
+/// Represent a socket address to which we may want to connect or bind.
 public enum SocketAddress: CustomStringConvertible {
     case v4(address: sockaddr_in, host: String)
     case v6(address: sockaddr_in6, host: String)
     case unixDomainSocket(address: sockaddr_un)
 
+    /// A human-readable description of this `SocketAddress`. Mostly useful for logging.
     public var description: String {
         let port: String
         let host: String?
@@ -53,6 +59,7 @@ public enum SocketAddress: CustomStringConvertible {
         return "[\(type)]\(host.map { "\($0):" } ?? "")\(port)"
     }
     
+    /// Returns the protocol family as defined in `man 2 socket` of this `SocketAddress`.
     public var protocolFamily: Int32 {
         switch self {
         case .v4(address: _, host: _):
@@ -64,14 +71,30 @@ public enum SocketAddress: CustomStringConvertible {
         }
     }
 
+    /// Creates a new IPV4 `SocketAddress`.
+    ///
+    /// - parameters:
+    ///       - addr: the `sockaddr_in` that holds the ipaddress and port.
+    ///       - host: the hostname that resolved to the ipaddress.
     public init(IPv4Address addr: sockaddr_in, host: String) {
         self = .v4(address: addr, host: host)
     }
 
+    /// Creates a new IPV6 `SocketAddress`.
+    ///
+    /// - parameters:
+    ///       - addr: the `sockaddr_in` that holds the ipaddress and port.
+    ///       - host: the hostname that resolved to the ipaddress.
     public init(IPv6Address addr: sockaddr_in6, host: String) {
         self = .v6(address: addr, host: host)
     }
 
+    /// Creates a new UDS `SocketAddress`.
+    ///
+    /// - parameters:
+    ///     - path: the path to use for the `SocketAddress`.
+    /// - returns: the `SocketAddress` for the given path.
+    /// - throws: may throw `SocketAddressError.unixDomainSocketPathTooLong` if the path is too long.
     public static func unixDomainSocketAddress(path: String) throws -> SocketAddress {
         guard path.utf8.count <= 103 else {
             throw SocketAddressError.unixDomainSocketPathTooLong
@@ -93,13 +116,19 @@ public enum SocketAddress: CustomStringConvertible {
         return .unixDomainSocket(address: addr)
     }
 
+    /// Creates a new `SocketAddress` for the given host (which will be resolved) and port.
+    ///
+    /// - parameters:
+    ///       - host: the hostname which should be resolved.
+    ///       - port: the port itself
+    /// - returns: the `SocketAddress` for the host / port pair.
+    /// - throws: a `SocketAddressError.unknown` if we could not resolve the `host`, or `SocketAddressError.unsupported` if the address itself is not supported (yet).
     public static func newAddressResolving(host: String, port: Int32) throws -> SocketAddress {
         var info: UnsafeMutablePointer<addrinfo>?
         
         /* FIXME: this is blocking! */
         if getaddrinfo(host, String(port), nil, &info) != 0 {
-            // TODO: May may be able to return a bit more info to the caller. Let us just keep it simple for now
-            throw SocketAddressError.unknown
+            throw SocketAddressError.unknown(host: host, port: port)
         }
         
         defer {
@@ -126,6 +155,5 @@ public enum SocketAddress: CustomStringConvertible {
             throw SocketAddressError.unsupported
         }
     }
-
 }
 
