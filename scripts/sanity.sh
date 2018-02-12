7d5dc67af8200ba51f1954b573fdd77fbe7c1373
Tom Doron <tomerd@apple.com>
2018-02-12 10:06:46 -0800

    scripts cleanup
    motivation: nicer source tree
    
    chanages:
    * move scripts to scripts directory
    * move integration tests to IntegrationTests directory and remove driver script
    * add .sh extension to bash scripts
    * adjust rio.yaml

--- scripts/sanity.sh
+++ scripts/sanity.sh
@@ -5,7 +5,7 @@ set -eu
 # check linux tests
 
 FIRST_OUT="$(git status --porcelain)"
-ruby generate_linux_tests.rb > /dev/null
+ruby scripts/generate_linux_tests.rb > /dev/null
 SECOND_OUT="$(git status --porcelain)"
 if [[ "$FIRST_OUT" != "$SECOND_OUT" ]]; then
   printf "\033[0;31mMissing linux test changes!\033[0m\n"
