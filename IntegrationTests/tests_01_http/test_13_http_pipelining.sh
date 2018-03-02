8891819faba062626199c63133d7d44a58d9a03b
Cory Benfield <lukasa@apple.com>
2018-03-02 01:12:17 +0000

    Add channel handler for server side pipelining.
    Motivation:
    
    HTTP pipelining can be tricky to handle properly on the server side.
    In particular, it's very easy to write out of order or inconsistently
    mutate state. Users often need help to handle this appropriately.
    
    Modifications:
    
    Added a HTTPServerPipelineHandler that only lets one request through
    at a time.
    
    Result:
    
    Better servers that are more able to handle HTTP pipelining

--- IntegrationTests/tests_01_http/test_13_http_pipelining.sh
+++ IntegrationTests/tests_01_http/test_13_http_pipelining.sh
@@ -28,77 +28,53 @@ echo -e 'GET /dynamic/count-to-ten HTTP/1.1\r\n\r\nGET /dynamic/count-to-ten HTT
 backslash_r=$(echo -ne '\r')
 cat > "$tmp/expected" <<EOF
 HTTP/1.1 200 OK$backslash_r
-Transfer-Encoding: chunked$backslash_r
-Connection: Keep-Alive$backslash_r
-Content-Type: text/plain$backslash_r
+transfer-encoding: chunked$backslash_r
 $backslash_r
+1$backslash_r
+1$backslash_r
+1$backslash_r
 2$backslash_r
-1
-$backslash_r
-2$backslash_r
-2
-$backslash_r
-2$backslash_r
-3
-$backslash_r
-2$backslash_r
-4
-$backslash_r
-2$backslash_r
-5
-$backslash_r
-2$backslash_r
-6
-$backslash_r
-2$backslash_r
-7
-$backslash_r
-2$backslash_r
-8
-$backslash_r
-2$backslash_r
-9
-$backslash_r
+1$backslash_r
 3$backslash_r
-10
-$backslash_r
+1$backslash_r
+4$backslash_r
+1$backslash_r
+5$backslash_r
+1$backslash_r
+6$backslash_r
+1$backslash_r
+7$backslash_r
+1$backslash_r
+8$backslash_r
+1$backslash_r
+9$backslash_r
+2$backslash_r
+10$backslash_r
 0$backslash_r
 $backslash_r
 HTTP/1.1 200 OK$backslash_r
-Transfer-Encoding: chunked$backslash_r
-Connection: Keep-Alive$backslash_r
-Content-Type: text/plain$backslash_r
+transfer-encoding: chunked$backslash_r
 $backslash_r
+1$backslash_r
+1$backslash_r
+1$backslash_r
 2$backslash_r
-1
-$backslash_r
-2$backslash_r
-2
-$backslash_r
-2$backslash_r
-3
-$backslash_r
-2$backslash_r
-4
-$backslash_r
-2$backslash_r
-5
-$backslash_r
-2$backslash_r
-6
-$backslash_r
-2$backslash_r
-7
-$backslash_r
-2$backslash_r
-8
-$backslash_r
-2$backslash_r
-9
-$backslash_r
+1$backslash_r
 3$backslash_r
-10
-$backslash_r
+1$backslash_r
+4$backslash_r
+1$backslash_r
+5$backslash_r
+1$backslash_r
+6$backslash_r
+1$backslash_r
+7$backslash_r
+1$backslash_r
+8$backslash_r
+1$backslash_r
+9$backslash_r
+2$backslash_r
+10$backslash_r
 0$backslash_r
 $backslash_r
 EOF
