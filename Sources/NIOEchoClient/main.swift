ad8911d6d648cf7dcdc0670b27a74f90026de1de
Ankit Aggarwal <ankit_aggarwal@apple.com>
2018-03-01 19:56:21 -0800

    Some random Swiftyness 2
    Some monotonic code style improvements

--- Sources/NIOEchoClient/main.swift
+++ Sources/NIOEchoClient/main.swift
@@ -35,15 +35,16 @@ private final class EchoHandler: ChannelInboundHandler {
     public func errorCaught(ctx: ChannelHandlerContext, error: Error) {
         print("error: ", error)
 
-        // As we are not really interested getting notified on success or failure we just pass nil as promise to
-        // reduce allocations.
+        // As we are not really interested getting notified on success or
+        // failure we just pass nil as promise to reduce allocations.
         ctx.close(promise: nil)
     }
     
     public func channelActive(ctx: ChannelHandlerContext) {
         print("Client connected to \(ctx.remoteAddress!)")
         
-        // We are connected its time to send the message to the server to initialize the ping-pong sequence.
+        // We are connected its time to send the message to the server to
+        // initialize the ping-pong sequence.
         var buffer = ctx.channel.allocator.buffer(capacity: line.utf8.count)
         buffer.write(string: line)
         self.numBytes = buffer.readableBytes
@@ -65,7 +66,7 @@ defer {
 // First argument is the program path
 let arguments = CommandLine.arguments
 let arg1 = arguments.dropFirst().first
-let arg2 = arguments.dropFirst().dropFirst().first
+let arg2 = arguments.dropFirst(2).first
 
 let defaultHost = "::1"
 let defaultPort: Int = 9999
@@ -76,16 +77,19 @@ enum ConnectTo {
 }
 
 let connectTarget: ConnectTo
-switch (arg1, arg1.flatMap { Int($0) }, arg2.flatMap { Int($0) }) {
-case (.some(let h), _ , .some(let p)):
-    /* we got two arguments, let's interpret that as host and port */
+switch (arg1, arg1.flatMap(Int.init), arg2.flatMap(Int.init)) {
+case (let h?, _ , let p?):
+    // We got two arguments, let's interpret that as host and port.
     connectTarget = .ip(host: h, port: p)
-case (.some(let portString), .none, _):
-    /* couldn't parse as number, expecting unix domain socket path */
+
+case (let portString?, .none, _):
+    // Couldn't parse as number, expecting unix domain socket path.
     connectTarget = .unixDomainSocket(path: portString)
-case (_, .some(let p), _):
-    /* only one argument --> port */
+
+case (_, let p?, _):
+    // Only one argument --> port.
     connectTarget = .ip(host: defaultHost, port: p)
+
 default:
     connectTarget = .ip(host: defaultHost, port: defaultPort)
 }
