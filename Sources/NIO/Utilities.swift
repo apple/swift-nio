9e046553602016ad4f227bad6b73daba0307127e
Cory Benfield <lukasa@apple.com>
2018-03-16 13:20:40 +0000

    Suppress compiler warnings in release mode.
    Motivation:
    
    There's no reason for us to emit compiler warnings.
    
    Modifications:
    
    Remove use of _isDebugAssertConfiguration to guard code, instead
    use a hand-rolled hacky solution on top of assert.
    
    Result:
    
    Fewer warnings, more sadness.

--- Sources/NIO/Utilities.swift
+++ Sources/NIO/Utilities.swift
@@ -28,6 +28,15 @@ private func darwinCoreCount() -> Int {
 }
 #endif
 
+/// A utility function that runs the body code only in debug builds, without
+/// emitting compiler warnings.
+///
+/// This is currently the only way to do this in Swift: see
+/// https://forums.swift.org/t/support-debug-only-code/11037 for a discussion.
+internal func debugOnly(_ body: () -> Void) {
+    assert({ body(); return true }())
+}
+
 /// Allows to "box" another value.
 final class Box<T> {
     let value: T
