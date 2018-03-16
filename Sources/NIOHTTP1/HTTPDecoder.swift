37ee542874c54693bd0134d792a79e5e755ad1c8
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-03-16 15:34:49 +0100

    Replace internal `HTTPResponseStatus` factory method with public init (#158)
    Motivation:
    
    In order to create a `HTTPResponseStatus` value from an status code code
    currently one would have to make a custom HTTPResponseStatus. The
    problem with that is that:
    `HTTPResponseStatus.custom(code: 200, reasonPhrase: "") != HTTPResponseStatus.ok`
    The logic of detecting whether a status code matches one of the known
    cases already exists but only in an internal factory method.
    
    Modifications:
    
    - removed internal static `from` method from `HTTPResponseStatus`
    - added public init with the functionality previously in `from`
    - replace the single use of the `from` method with the new `init`
    
    Result:
    
    When initializing a `HTTPResponseStatus` value with a value of e.g.
    `200` the init method will result in a `HTTPResponseStatus.ok`.

--- Sources/NIOHTTP1/HTTPDecoder.swift
+++ Sources/NIOHTTP1/HTTPDecoder.swift
@@ -202,7 +202,7 @@ public class HTTPDecoder<HTTPMessageT>: ByteToMessageDecoder, AnyHTTPDecoder {
     }
 
     private func newResponseHead(_ parser: UnsafeMutablePointer<http_parser>!) -> HTTPResponseHead {
-        let status = HTTPResponseStatus.from(parser.pointee.status_code, state.currentStatus!)
+        let status = HTTPResponseStatus(statusCode: Int(parser.pointee.status_code), reasonPhrase: state.currentStatus!)
         let version = HTTPVersion(major: parser.pointee.http_major, minor: parser.pointee.http_minor)
         let response = HTTPResponseHead(version: version, status: status, headers: state.currentHeaders ?? HTTPHeaders())
         state.currentHeaders = nil
@@ -574,139 +574,6 @@ extension HTTPMethod {
             return .UNLINK
         default:
             fatalError("Unexpected http_method \(httpParserMethod)")
-        }
-    }
-}
-
-extension HTTPResponseStatus {
-    /// Create a `HTTPResponseStatus` from a given status and reason produced by
-    /// `http_parser`.
-    ///
-    /// - Parameter statusCode: The integer value of the HTTP response status code
-    /// - Parameter reasonPhrase: The textual reason phrase from the response.
-    /// - Returns: The corresponding `HTTPResponseStatus`.
-    static func from(_ statusCode: UInt32, _ reasonPhrase: String) -> HTTPResponseStatus {
-        switch statusCode {
-        case 100:
-            return .`continue`
-        case 101:
-            return .switchingProtocols
-        case 102:
-            return .processing
-        case 200:
-            return .ok
-        case 201:
-            return .created
-        case 202:
-            return .accepted
-        case 203:
-            return .nonAuthoritativeInformation
-        case 204:
-            return .noContent
-        case 205:
-            return .resetContent
-        case 206:
-            return .partialContent
-        case 207:
-            return .multiStatus
-        case 208:
-            return .alreadyReported
-        case 226:
-            return .imUsed
-        case 300:
-            return .multipleChoices
-        case 301:
-            return .movedPermanently
-        case 302:
-            return .found
-        case 303:
-            return .seeOther
-        case 304:
-            return .notModified
-        case 305:
-            return .useProxy
-        case 307:
-            return .temporaryRedirect
-        case 308:
-            return .permanentRedirect
-        case 400:
-            return .badRequest
-        case 401:
-            return .unauthorized
-        case 402:
-            return .paymentRequired
-        case 403:
-            return .forbidden
-        case 404:
-            return .notFound
-        case 405:
-            return .methodNotAllowed
-        case 406:
-            return .notAcceptable
-        case 407:
-            return .proxyAuthenticationRequired
-        case 408:
-            return .requestTimeout
-        case 409:
-            return .conflict
-        case 410:
-            return .gone
-        case 411:
-            return .lengthRequired
-        case 412:
-            return .preconditionFailed
-        case 413:
-            return .payloadTooLarge
-        case 414:
-            return .uriTooLong
-        case 415:
-            return .unsupportedMediaType
-        case 416:
-            return .rangeNotSatisfiable
-        case 417:
-            return .expectationFailed
-        case 421:
-            return .misdirectedRequest
-        case 422:
-            return .unprocessableEntity
-        case 423:
-            return .locked
-        case 424:
-            return .failedDependency
-        case 426:
-            return .upgradeRequired
-        case 428:
-            return .preconditionRequired
-        case 429:
-            return .tooManyRequests
-        case 431:
-            return .requestHeaderFieldsTooLarge
-        case 451:
-            return .unavailableForLegalReasons
-        case 500:
-            return .internalServerError
-        case 501:
-            return .notImplemented
-        case 502:
-            return .badGateway
-        case 503:
-            return .serviceUnavailable
-        case 504:
-            return .gatewayTimeout
-        case 505:
-            return .httpVersionNotSupported
-        case 506:
-            return .variantAlsoNegotiates
-        case 507:
-            return .insufficientStorage
-        case 508:
-            return .loopDetected
-        case 510:
-            return .notExtended
-        case 511:
-            return .networkAuthenticationRequired
-        default:
-            return .custom(code: UInt(statusCode), reasonPhrase: reasonPhrase)
         }
     }
 }
