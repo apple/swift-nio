dbe321424d8b8bad0ecb75dd4c2b997773cb7e00
Frank Kair <frankkair@gmail.com>
2018-03-27 15:36:45 -0300

    Refactors generate_linux_tests script (#236)
    Motivation:
    
    generate_linux_tests.rb is written in a non idiomatic way.
    
    Modifications:
    
    I used Rubocop to refactor the code.
    More info: https://github.com/bbatsov/rubocop
    
    Result:
    
    A more idiomatic Ruby script.

--- scripts/generate_linux_tests.rb
+++ scripts/generate_linux_tests.rb
@@ -31,7 +31,7 @@ include FileUtils
 # See https://github.com/apple/swift-corelibs-xctest/blob/master/Documentation/Linux.md
 #
 def header(fileName)
-    string = <<-eos
+  string = <<-eos
 //===----------------------------------------------------------------------===//
 //
 // This source file is part of the SwiftNIO open source project
@@ -57,126 +57,122 @@ import XCTest
 ///
     eos
 
-    return string
-              .sub("<FileName>", File.basename(fileName))
-              .sub("<Date>", Time.now.to_s)
+  string
+    .sub('<FileName>', File.basename(fileName))
+    .sub('<Date>', Time.now.to_s)
 end
 
-
 def createExtensionFile(fileName, classes)
-    extensionFile = fileName.sub! ".swift", "+XCTest.swift"
-    print "Creating file: " + extensionFile + "\n"
+  extensionFile = fileName.sub! '.swift', '+XCTest.swift'
+  print 'Creating file: ' + extensionFile + "\n"
 
-     File.open(extensionFile, 'w') { |file|
+  File.open(extensionFile, 'w') do |file|
+    file.write header(extensionFile)
+    file.write "\n"
 
-         file.write header(extensionFile)
-         file.write "\n"
+    for classArray in classes
+      file.write 'extension ' + classArray[0] + " {\n\n"
+      file.write '   static var allTests : [(String, (' + classArray[0] + ") -> () throws -> Void)] {\n"
+      file.write "      return [\n"
 
-         for classArray in classes
-             file.write "extension " + classArray[0] + " {\n\n"
-             file.write "   static var allTests : [(String, (" + classArray[0] + ") -> () throws -> Void)] {\n"
-             file.write "      return [\n"
+      for funcName in classArray[1]
+        file.write '                ("' + funcName + '", ' + funcName + "),\n"
+      end
 
-             for funcName in classArray[1]
-                 file.write "                (\"" + funcName + "\", " + funcName + "),\n"
-             end
-
-             file.write "           ]\n"
-             file.write "   }\n"
-             file.write "}\n\n"
-         end
-     }
+      file.write "           ]\n"
+      file.write "   }\n"
+      file.write "}\n\n"
+    end
+  end
 end
 
 def createLinuxMain(testsDirectory, allTestSubDirectories, files)
-    fileName = testsDirectory + "/LinuxMain.swift"
-    print "Creating file: " + fileName + "\n"
+  fileName = testsDirectory + '/LinuxMain.swift'
+  print 'Creating file: ' + fileName + "\n"
 
-    File.open(fileName, 'w') { |file|
+  File.open(fileName, 'w') do |file|
+    file.write header(fileName)
+    file.write "\n"
 
-        file.write header(fileName)
-        file.write "\n"
+    file.write "#if os(Linux) || os(FreeBSD)\n"
+    for testSubDirectory in allTestSubDirectories.sort { |x, y| x <=> y }
+      file.write '   @testable import ' + testSubDirectory + "\n"
+    end
+    file.write "\n"
+    file.write "   XCTMain([\n"
 
-        file.write "#if os(Linux) || os(FreeBSD)\n"
-        for testSubDirectory in allTestSubDirectories.sort { |x,y| x <=> y }
-            file.write "   @testable import " + testSubDirectory + "\n"
-        end
-        file.write "\n"
-        file.write "   XCTMain([\n"
+    testCases = []
+    for classes in files
+      for classArray in classes
+        testCases << classArray[0]
+      end
+    end
 
-        testCases = []
-        for classes in files
-            for classArray in classes
-                testCases << classArray[0]
-            end
-        end
-
-        for testCase in testCases.sort { |x,y| x <=> y }
-            file.write "         testCase(" + testCase + ".allTests),\n"
-        end
-        file.write"    ])\n"
-        file.write "#endif\n"
-    }
+    for testCase in testCases.sort { |x, y| x <=> y }
+      file.write '         testCase(' + testCase + ".allTests),\n"
+    end
+    file.write "    ])\n"
+    file.write "#endif\n"
+  end
 end
 
 def parseSourceFile(fileName)
-    puts "Parsing file:  " + fileName + "\n"
+  puts 'Parsing file:  ' + fileName + "\n"
 
-    classes = Array.new
-    currentClass = nil
-    inIfLinux = false
-    inElse    = false
-    ignore    = false
+  classes = []
+  currentClass = nil
+  inIfLinux = false
+  inElse    = false
+  ignore    = false
 
-    #
-    # Read the file line by line
-    # and parse to find the class
-    # names and func names
-    #
-    File.readlines(fileName).each do |line|
-        if inIfLinux
-            if /\#else/.match(line)
-                inElse = true
-                ignore = true
-                else
-                if /\#end/.match(line)
-                    inElse = false
-                    inIfLinux = false
-                    ignore = false
-                end
-            end
-            else
-            if /\#if[ \t]+os\(Linux\)/.match(line)
-                inIfLinux = true
-                ignore = false
-            end
+  #
+  # Read the file line by line
+  # and parse to find the class
+  # names and func names
+  #
+  File.readlines(fileName).each do |line|
+    if inIfLinux
+      if /\#else/.match(line)
+        inElse = true
+        ignore = true
+      else
+        if /\#end/.match(line)
+          inElse = false
+          inIfLinux = false
+          ignore = false
         end
-
-        if !ignore
-            # Match class or func
-            match = line[/class[ \t]+[a-zA-Z0-9_]*(?=[ \t]*:[ \t]*XCTestCase)|func[ \t]+test[a-zA-Z0-9_]*(?=[ \t]*\(\))/, 0]
-            if match
-
-                if match[/class/, 0] == "class"
-                    className = match.sub(/^class[ \t]+/, '')
-                    #
-                    # Create a new class / func structure
-                    # and add it to the classes array.
-                    #
-                    currentClass = [className, Array.new]
-                    classes << currentClass
-                else # Must be a func
-                    funcName  = match.sub(/^func[ \t]+/, '')
-                    #
-                    # Add each func name the the class / func
-                    # structure created above.
-                    #
-                    currentClass[1] << funcName
-                end
-            end
+        end
+    else
+      if /\#if[ \t]+os\(Linux\)/.match(line)
+        inIfLinux = true
+        ignore = false
         end
     end
-    return classes
+
+    next if ignore
+    # Match class or func
+    match = line[/class[ \t]+[a-zA-Z0-9_]*(?=[ \t]*:[ \t]*XCTestCase)|func[ \t]+test[a-zA-Z0-9_]*(?=[ \t]*\(\))/, 0]
+    if match
+
+      if match[/class/, 0] == 'class'
+        className = match.sub(/^class[ \t]+/, '')
+        #
+        # Create a new class / func structure
+        # and add it to the classes array.
+        #
+        currentClass = [className, []]
+        classes << currentClass
+      else # Must be a func
+        funcName = match.sub(/^func[ \t]+/, '')
+        #
+        # Add each func name the the class / func
+        # structure created above.
+        #
+        currentClass[1] << funcName
+      end
+    end
+  end
+  classes
 end
 
 #
@@ -184,48 +180,45 @@ end
 #
 #
 
-testsDirectory="Tests"
+testsDirectory = 'Tests'
 
-options = GetoptLong.new([ '--tests-dir', GetoptLong::OPTIONAL_ARGUMENT ])
+options = GetoptLong.new(['--tests-dir', GetoptLong::OPTIONAL_ARGUMENT])
 options.quiet = true
 
 begin
-    options.each do |option, value|
-        case option
-            when '--tests-dir'
-            testsDirectory=value
-        end
+  options.each do |option, value|
+    case option
+    when '--tests-dir'
+      testsDirectory = value
     end
-    rescue GetoptLong::InvalidOption
+  end
+rescue GetoptLong::InvalidOption
 end
 
-allTestSubDirectories = Array.new
-allFiles = Array.new
+allTestSubDirectories = []
+allFiles = []
 
 Dir[testsDirectory + '/*'].each do |subDirectory|
-    if File.directory?(subDirectory)
-        directoryHasClasses = false
-        Dir[subDirectory + '/*Test{s,}.swift'].each do |fileName|
-            if File.file? fileName
-                fileClasses = parseSourceFile(fileName)
+  next unless File.directory?(subDirectory)
+  directoryHasClasses = false
+  Dir[subDirectory + '/*Test{s,}.swift'].each do |fileName|
+    next unless File.file? fileName
+    fileClasses = parseSourceFile(fileName)
 
-                #
-                # If there are classes in the
-                # test source file, create an extension
-                # file for it.
-                #
-                if fileClasses.count > 0
-                    createExtensionFile(fileName, fileClasses)
-                    directoryHasClasses = true
-                    allFiles << fileClasses
-                end
-            end
-        end
+    #
+    # If there are classes in the
+    # test source file, create an extension
+    # file for it.
+    #
+    next unless fileClasses.count > 0
+    createExtensionFile(fileName, fileClasses)
+    directoryHasClasses = true
+    allFiles << fileClasses
+  end
 
-        if directoryHasClasses
-            allTestSubDirectories << Pathname.new(subDirectory).split.last.to_s
-        end
-    end
+  if directoryHasClasses
+    allTestSubDirectories << Pathname.new(subDirectory).split.last.to_s
+  end
 end
 
 #
@@ -233,6 +226,6 @@ end
 # references all the classes and funcs in the source files.
 #
 if allFiles.count > 0
-    createLinuxMain(testsDirectory, allTestSubDirectories, allFiles)
+  createLinuxMain(testsDirectory, allTestSubDirectories, allFiles)
 end
 # eof
