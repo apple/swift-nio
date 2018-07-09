13b36ce6b2157cf61f3b5ee5cbe0e2f0bd6c1395
Esteban Torres <me@estebantorr.es>
2018-07-09 15:24:58 +0200

    Address @weissi comments

--- Sources/NIO/CircularBuffer.swift
+++ Sources/NIO/CircularBuffer.swift
@@ -23,8 +23,7 @@ public protocol AppendableCollection: Collection {
 /// An automatically expanding ring buffer implementation backed by a `ContiguousArray`. Even though this implementation
 /// will automatically expand if more elements than `initialRingCapacity` are stored, it's advantageous to prevent
 /// expansions from happening frequently. Expansions will always force an allocation and a copy to happen.
-public struct CircularBuffer<E>: CustomStringConvertible, AppendableCollection
-{
+public struct CircularBuffer<E>: CustomStringConvertible, AppendableCollection {
     // this typealias is so complicated because of SR-6963, when that's fixed we can drop the generic parameters and the where clause
     #if swift(>=4.2)
     public typealias RangeType<Bound> = Range<Bound> where Bound: Strideable, Bound.Stride: SignedInteger
@@ -178,7 +177,7 @@ public struct CircularBuffer<E>: CustomStringConvertible, AppendableCollection
     /// Returns the index before `index`.
     public func index(before: Int) -> Int {
         precondition(before > 0)
-        return (before - 1)
+        return before - 1
     }
 
     /// Removes all members from the circular buffer whist keeping the capacity.
