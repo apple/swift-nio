e00c0d3f5857507599ef584849f2bbd6352619ae
Ole Begemann <ole@oleb.net>
2018-08-01 16:00:49 +0200

    Remove a stray space from the end of an error message
    Motivation:
    
    The stray space looks like an oversight.
    
    Modifications:
    
    Removed a stray space from the end of a string interpolation literal
    that's used to construct an error message.
    
    Result:
    
    The exact value (but not the meaning) of the error message string
    changes. Clients that match against the exact error message might break,
    but that's arguably bad practice.

--- Sources/NIO/IO.swift
+++ Sources/NIO/IO.swift
@@ -60,7 +60,7 @@ private func reasonForError(errnoCode: Int32, reason: String) -> String {
         let errorDescLen = strlen(errorDescC)
         return errorDescC.withMemoryRebound(to: UInt8.self, capacity: errorDescLen) { ptr in
             let errorDescPtr = UnsafeBufferPointer<UInt8>(start: ptr, count: errorDescLen)
-            return "\(reason): \(String(decoding: errorDescPtr, as: UTF8.self)) (errno: \(errnoCode)) "
+            return "\(reason): \(String(decoding: errorDescPtr, as: UTF8.self)) (errno: \(errnoCode))"
         }
     } else {
         return "\(reason): Broken strerror, unknown error: \(errnoCode)"
