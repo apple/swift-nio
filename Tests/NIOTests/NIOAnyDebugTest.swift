ae3525b28c9c1303fb1780a6b7f16f0a05d16090
Johannes Weiss <johannesweiss@apple.com>
2019-03-04 19:25:31 +0000

    Update NIOAnyDebugTest.swift

--- Tests/NIOTests/NIOAnyDebugTest.swift
+++ Tests/NIOTests/NIOAnyDebugTest.swift
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) YEARS Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import XCTest
 import NIO
 
