a9811972ca8e9d7893993f0fc7b64c53accb1127
Kevin A. Hoogheem <kevinh@npe-inc.com>
2019-03-07 15:07:34 -0600

    Check if loopback supports Multicast Check nil on the ipv4DatagramChannel in teardown

--- Tests/NIOTests/SocketOptionProviderTest.swift
+++ Tests/NIOTests/SocketOptionProviderTest.swift
@@ -67,22 +67,28 @@ final class SocketOptionProviderTest: XCTestCase {
         let v6LoopbackAddress = try! SocketAddress(ipAddress: "::1", port: 0)
         let v4LoopbackInterface = try! System.enumerateInterfaces().filter { $0.address == v4LoopbackAddress }.first!
 
-        self.ipv4DatagramChannel = try? assertNoThrowWithValue(
-            DatagramBootstrap(group: group).bind(host: "127.0.0.1", port: 0).flatMap { channel in
-                return (channel as! MulticastChannel).joinGroup(try! SocketAddress(ipAddress: "224.0.2.66", port: 0), interface: v4LoopbackInterface).map { channel }
-            }.wait()
-        )
+        // Only run The setup if the Loopback supports multicast
+        if v4LoopbackAddress.isMulticast {
+            self.ipv4DatagramChannel = try? assertNoThrowWithValue(
+                DatagramBootstrap(group: group).bind(host: "127.0.0.12", port: 0).flatMap { channel in
+                    return (channel as! MulticastChannel).joinGroup(try! SocketAddress(ipAddress: "224.0.2.66", port: 0), interface: v4LoopbackInterface).map { channel }
+                    }.wait()
+            )
+        }
 
-        // The IPv6 setup is allowed to fail, some hosts don't have IPv6.
-       let v6LoopbackInterface = try! System.enumerateInterfaces().filter { $0.address == v6LoopbackAddress }.first
-        self.ipv6DatagramChannel = try? DatagramBootstrap(group: group).bind(host: "::1", port: 0).flatMap { channel in
-            return (channel as! MulticastChannel).joinGroup(try! SocketAddress(ipAddress: "ff12::beeb", port: 0), interface: v6LoopbackInterface).map { channel }
-        }.wait()
+        // Only run The setup if the Loopback supports multicast
+        if v6LoopbackAddress.isMulticast {
+            // The IPv6 setup is allowed to fail, some hosts don't have IPv6.
+            let v6LoopbackInterface = try! System.enumerateInterfaces().filter { $0.address == v6LoopbackAddress }.first
+            self.ipv6DatagramChannel = try? DatagramBootstrap(group: group).bind(host: "::1", port: 0).flatMap { channel in
+                return (channel as! MulticastChannel).joinGroup(try! SocketAddress(ipAddress: "ff12::beeb", port: 0), interface: v6LoopbackInterface).map { channel }
+                }.wait()
+        }
     }
 
     override func tearDown() {
         XCTAssertNoThrow(try ipv6DatagramChannel?.close().wait())
-        XCTAssertNoThrow(try ipv4DatagramChannel.close().wait())
+        XCTAssertNoThrow(try ipv4DatagramChannel?.close().wait())
         XCTAssertNoThrow(try clientChannel.close().wait())
         XCTAssertNoThrow(try serverChannel.close().wait())
         XCTAssertNoThrow(try group.syncShutdownGracefully())
