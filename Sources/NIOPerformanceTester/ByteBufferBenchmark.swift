afbac231403c8bde6a4b773d789f4c62682d37f6
Richard Low <ral@apple.com>
2019-11-11 08:56:35 +0000

    Add a benchmark for CircularBuffer<UInt8> setBytes/writeBytes, see #1216.
    Motivation:
    
    When calling ByteBuffer setBytes/writeBytes with CircularBuffer<Uint8> we take the slow path. Before we make any changes we want a baseline for current performance.
    
    Modifications:
    
    Adds ByteBuferBenchmark.swift and tests circular_buffer_into_byte_buffer_1kb, circular_buffer_into_byte_buffer_1mb.
    
    Result:
    
    Extra performance tests

---
+++ Sources/NIOPerformanceTester/ByteBufferBenchmark.swift
@@ -0,0 +1,47 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2019 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+import NIO
+
+final class ByteBufferBenchmark: Benchmark {
+    private let iterations : Int
+    private let bufferSize : Int
+    private var circularBuffer: CircularBuffer<UInt8>
+    private var buffer: ByteBuffer
+
+    init(iterations: Int, bufferSize: Int) {
+        self.iterations = iterations
+        self.bufferSize = bufferSize
+        self.circularBuffer = CircularBuffer<UInt8>(initialCapacity: self.bufferSize)
+        self.buffer = ByteBufferAllocator().buffer(capacity: self.bufferSize)
+    }
+
+    func setUp() throws {
+        for i in 0..<self.bufferSize {
+            self.circularBuffer.append(UInt8(i % 256))
+        }
+    }
+
+    func tearDown() {
+    }
+
+    func run() -> Int {
+        for _ in 1...self.iterations {
+            self.buffer.writeBytes(self.circularBuffer)
+            self.buffer.setBytes(self.circularBuffer, at: 0)
+        }
+        return 1
+    }
+}
