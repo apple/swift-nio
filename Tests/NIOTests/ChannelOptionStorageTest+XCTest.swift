0d1d1660dc77854cc346b9c92fcb70985295e100
David Evans <david.evans@blackpoint.co>
2019-11-20 14:51:20 +0000

    Define convenience socket options

--- Tests/NIOTests/ChannelOptionStorageTest+XCTest.swift
+++ Tests/NIOTests/ChannelOptionStorageTest+XCTest.swift
@@ -30,6 +30,7 @@ extension ChannelOptionStorageTest {
                 ("testSetTwoOptionsOfDifferentType", testSetTwoOptionsOfDifferentType),
                 ("testSetTwoOptionsOfSameType", testSetTwoOptionsOfSameType),
                 ("testSetOneOptionTwice", testSetOneOptionTwice),
+                ("testConvenienceSocketOptions", testConvenienceSocketOptions)
            ]
    }
 }
