de038691808a95a3a8f1ba2cf91b009d368fdf7c
Richard Low <ral@apple.com>
2019-11-29 09:44:20 +0000

    Update CodecTest+XCTest.swift

--- Tests/NIOTests/CodecTest+XCTest.swift
+++ Tests/NIOTests/CodecTest+XCTest.swift
@@ -78,6 +78,7 @@ extension MessageToByteEncoderTest {
 
 extension NIOSingleStepByteToMessageDecoderTest {
 
+   @available(*, deprecated, message: "not actually deprecated. Just deprecated to allow deprecated tests (which test deprecated functionality) without warnings")
    static var allTests : [(String, (NIOSingleStepByteToMessageDecoderTest) -> () throws -> Void)] {
       return [
                 ("testDecoder", testDecoder),
