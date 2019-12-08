dff550f43dd53dbfc974743adc1ed74faed78e5c
Trevör Anne Denise <trevor.annedenise@icloud.com>
2019-12-08 23:21:36 +0100

    Use actual element count of PriorityQueue for its description

--- Sources/NIO/PriorityQueue.swift
+++ Sources/NIO/PriorityQueue.swift
@@ -77,6 +77,6 @@ extension PriorityQueue {
 
 extension PriorityQueue: CustomStringConvertible {
     var description: String {
-        return "PriorityQueue(count: \(self.underestimatedCount)): \(Array(self))"
+        return "PriorityQueue(count: \(self.count)): \(Array(self))"
     }
 }
