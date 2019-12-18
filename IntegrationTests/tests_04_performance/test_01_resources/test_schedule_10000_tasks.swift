7314c597338958e7be49de32e9064ccc1a256229
Johannes Weiss <johannesweiss@apple.com>
2019-12-18 22:27:12 +0000

    add an allocation counter test for scheduling tasks (#1315)
    * add an allocation counter test for scheduling tasks
    
    Motivation:
    
    Scheduling tasks is important so we should have an allocation counter
    test.
    
    Modifications:
    
    add one
    
    Result:
    
    more alloc counter tests
    
    * Update test_schedule_10000_tasks.swift
    
    * Update docker-compose.1804.50.yaml
    
    * Update docker-compose.1604.51.yaml

---
+++ IntegrationTests/tests_04_performance/test_01_resources/test_schedule_10000_tasks.swift
@@ -0,0 +1,35 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2019 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Dispatch
+import NIO
+
+func run(identifier: String) {
+    let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+    let loop = group.next()
+    let dg = DispatchGroup()
+
+    measure(identifier: identifier) {
+        loop.execute {
+            for _ in 0..<10_000 {
+                dg.enter()
+                loop.scheduleTask(in: .nanoseconds(0)) { dg.leave() }
+            }
+        }
+        dg.wait()
+        return 10_000
+    }
+
+    try! group.syncShutdownGracefully()
+}
