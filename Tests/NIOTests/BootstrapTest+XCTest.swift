ba37512da979d53d3cfc7503d2616ce2d85169a1
Johannes Weiss <johannesweiss@apple.com>
2020-02-01 10:21:54 +0000

    apply ChannelOptions before channel initializer runs (#1372)
    Motivation:
    
    The channel initializers should see the the Channel after the options
    have been applied so they can potentially override the options.
    
    Modifications:
    
    Set the options before the channel initialiser rather than after.
    
    Result:
    
    - More sense, more compatibility with Netty
    - fixes #1114
    
    Co-authored-by: George Barnett <gbrntt@gmail.com>
    Co-authored-by: Cory Benfield <lukasa@apple.com>

--- Tests/NIOTests/BootstrapTest+XCTest.swift
+++ Tests/NIOTests/BootstrapTest+XCTest.swift
@@ -34,6 +34,11 @@ extension BootstrapTest {
                 ("testPreConnectedServerSocketToleratesFuturesFromDifferentEventLoopsReturnedInInitializers", testPreConnectedServerSocketToleratesFuturesFromDifferentEventLoopsReturnedInInitializers),
                 ("testTCPClientBootstrapAllowsConformanceCorrectly", testTCPClientBootstrapAllowsConformanceCorrectly),
                 ("testServerBootstrapBindTimeout", testServerBootstrapBindTimeout),
+                ("testServerBootstrapSetsChannelOptionsBeforeChannelInitializer", testServerBootstrapSetsChannelOptionsBeforeChannelInitializer),
+                ("testClientBootstrapSetsChannelOptionsBeforeChannelInitializer", testClientBootstrapSetsChannelOptionsBeforeChannelInitializer),
+                ("testPreConnectedSocketSetsChannelOptionsBeforeChannelInitializer", testPreConnectedSocketSetsChannelOptionsBeforeChannelInitializer),
+                ("testDatagramBootstrapSetsChannelOptionsBeforeChannelInitializer", testDatagramBootstrapSetsChannelOptionsBeforeChannelInitializer),
+                ("testPipeBootstrapSetsChannelOptionsBeforeChannelInitializer", testPipeBootstrapSetsChannelOptionsBeforeChannelInitializer),
            ]
    }
 }
