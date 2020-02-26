fdbfada1331e6917e3401b09c42372652e43088b
Kyle Browning <kylebrowning@me.com>
2020-02-26 10:03:16 -0800

    Allow discardable results on NIOAtomic for mutating functions

--- Sources/NIOConcurrencyHelpers/NIOAtomic.swift
+++ Sources/NIOConcurrencyHelpers/NIOAtomic.swift
@@ -223,6 +223,7 @@ public final class NIOAtomic<T: NIOAtomicPrimitive> {
     /// - Returns: `True` if the exchange occurred, or `False` if `expected` did not
     ///     match the current value and so no exchange occurred.
     @inlinable
+    @discardableResult
     public func compareAndExchange(expected: T, desired: T) -> Bool {
         return Manager(unsafeBufferObject: self).withUnsafeMutablePointerToElements {
             return T.nio_atomic_compare_and_exchange($0, expected, desired)
@@ -238,6 +239,7 @@ public final class NIOAtomic<T: NIOAtomicPrimitive> {
     /// - Parameter rhs: The value to add to this object.
     /// - Returns: The previous value of this object, before the addition occurred.
     @inlinable
+    @discardableResult
     public func add(_ rhs: T) -> T {
         return Manager(unsafeBufferObject: self).withUnsafeMutablePointerToElements {
             return T.nio_atomic_add($0, rhs)
@@ -253,6 +255,7 @@ public final class NIOAtomic<T: NIOAtomicPrimitive> {
     /// - Parameter rhs: The value to subtract from this object.
     /// - Returns: The previous value of this object, before the subtraction occurred.
     @inlinable
+    @discardableResult
     public func sub(_ rhs: T) -> T {
         return Manager(unsafeBufferObject: self).withUnsafeMutablePointerToElements {
             return T.nio_atomic_sub($0, rhs)
@@ -268,6 +271,7 @@ public final class NIOAtomic<T: NIOAtomicPrimitive> {
     /// - Parameter value: The new value to set this object to.
     /// - Returns: The value previously held by this object.
     @inlinable
+    @discardableResult
     public func exchange(with value: T) -> T {
         return Manager(unsafeBufferObject: self).withUnsafeMutablePointerToElements {
             return T.nio_atomic_exchange($0, value)
