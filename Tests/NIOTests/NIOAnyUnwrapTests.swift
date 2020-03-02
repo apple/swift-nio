569e5671be26ec66b6d239009437e997c4dc9563
David Evans <david.evans@blackpoint.co>
2020-03-02 15:45:11 +0000

    Tests

---
+++ Tests/NIOTests/NIOAnyUnwrapTests.swift
@@ -0,0 +1,31 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) YEARS Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import XCTest
+import NIO
+
+class NIOAnyUnwrapTests: XCTestCase {
+    
+    func testSafeUnwrap_some() {
+        let value = "value"
+        let data = NIOAny(value)
+        XCTAssertEqual(data.unwrap(as: String.self), value)
+    }
+    
+    func testSafeUnwrap_nil() {
+        let data = NIOAny("value")
+        XCTAssertNil(data.unwrap(as: Int.self))
+    }
+    
+}
