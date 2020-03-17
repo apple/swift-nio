2673be6ae332cce5e86e63be688c85a616d7fb7c
Saleem Abdulrasool <compnerd@compnerd.org>
2020-03-17 02:31:00 -0700

    NIO: move comment to right location (NFC) (#1452)
    Move the block comment to the right location before the function it
    describes.

--- Sources/NIO/Utilities.swift
+++ Sources/NIO/Utilities.swift
@@ -12,11 +12,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-/// A utility function that runs the body code only in debug builds, without
-/// emitting compiler warnings.
-///
-/// This is currently the only way to do this in Swift: see
-/// https://forums.swift.org/t/support-debug-only-code/11037 for a discussion.
 import CNIOLinux
 
 #if os(Windows)
@@ -30,6 +25,11 @@ import struct WinSDK.SYSTEM_LOGICAL_PROCESSOR_INFORMATION
 import typealias WinSDK.DWORD
 #endif
 
+/// A utility function that runs the body code only in debug builds, without
+/// emitting compiler warnings.
+///
+/// This is currently the only way to do this in Swift: see
+/// https://forums.swift.org/t/support-debug-only-code/11037 for a discussion.
 @inlinable
 internal func debugOnly(_ body: () -> Void) {
     assert({ body(); return true }())
