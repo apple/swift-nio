ad419b56d4db500c3fdf16b5455ee7a86d6088c0
Saleem Abdulrasool <compnerd@compnerd.org>
2020-03-21 10:51:16 -0700

    NIO: add module constructor for Windows (#1450)
    This adds the necessary code to add a module constructor for NIO.  On
    Windows, the socketing subsystem (WinSock) must be explicitly
    initialized unlike Unices.  Add a module constructor that will be
    handled by the C runtime to initialize the subsystem before the module
    code is executed so that the user does not need to worry about this.
    
    Since there is no way to inject this in Swift, write a tiny helper in C
    and link that into the final NIO binary.
    
    Since swift-package-manager does not support mixed-language directories,
    add this to `CNIOWindows` which will need to be statically linked and
    must always be imported as `@_implementationOnly` as this cannot be
    linked into any user libraries.
    
    Co-authored-by: Cory Benfield <lukasa@apple.com>

---
+++ Sources/CNIOWindows/WSAStartup.c
@@ -0,0 +1,31 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2020 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+#include <Winsock2.h>
+
+#include <stdlib.h>
+
+#pragma section(".CRT$XCU", read)
+
+static void __cdecl
+NIOWSAStartup(void) {
+    WSADATA wsa;
+    WORD wVersionRequested = MAKEWORD(2, 2);
+    if (!WSAStartup(wVersionRequested, &wsa)) {
+        _exit(EXIT_FAILURE);
+    }
+}
+
+__declspec(allocate(".CRT$XCU"))
+static void (*pNIOWSAStartup)(void) = &NIOWSAStartup;
