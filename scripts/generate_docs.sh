677e4e452f74fe3b7c79794249873a9d47846099
Johannes Weiss <johannesweiss@apple.com>
2020-04-17 18:08:22 +0100

    link swift-nio-extras in docs

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -86,6 +86,7 @@ For the API documentation of the other repositories in the SwiftNIO family check
 - [`swift-nio` API docs](https://apple.github.io/swift-nio/docs/current/NIO/index.html)
 - [`swift-nio-ssl` API docs](https://apple.github.io/swift-nio-ssl/docs/current/NIOSSL/index.html)
 - [`swift-nio-http2` API docs](https://apple.github.io/swift-nio-http2/docs/current/NIOHTTP2/index.html)
+- [`swift-nio-extras` API docs](https://apple.github.io/swift-nio-extras/docs/current/NIOExtras/index.html)
 
 EOF
 
