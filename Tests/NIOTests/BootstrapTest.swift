eff9c043d0d0e88a9ac8ee987fdbc0ec3daf8857
Peter Adams <pp_adams@apple.com>
2020-04-20 09:31:32 +0100

    Add test for universal bootstrap equivalence.

--- Tests/NIOTests/BootstrapTest.swift
+++ Tests/NIOTests/BootstrapTest.swift
@@ -620,6 +620,51 @@ class BootstrapTest: XCTestCase {
                                    shortOption: .allowRemoteHalfClosure)
     }
     
+    func testShorthandOptionsAreEquivalentUniversalClient() throws {
+        func setAndGetOption<Option>(option: Option, _ applyOptions : (NIOClientTCPBootstrap) -> NIOClientTCPBootstrap) throws
+            -> Option.Value where Option : ChannelOption {
+            var optionRead : EventLoopFuture<Option.Value>?
+            XCTAssertNoThrow(try withTCPServerChannel(group: self.group) { server in
+                var channel: Channel? = nil
+                XCTAssertNoThrow(channel = try applyOptions(NIOClientTCPBootstrap(
+                    ClientBootstrap(group: self.group), tls: NIOInsecureNoTLS()))
+                    .channelInitializer { channel in optionRead = channel.getOption(option)
+                        return channel.eventLoop.makeSucceededFuture(())
+                    }
+                .connect(to: server.localAddress!)
+                .wait())
+                XCTAssertNotNil(optionRead)
+                XCTAssertNotNil(channel)
+                XCTAssertNoThrow(try channel?.close().wait())
+            })
+            return try optionRead!.wait()
+        }
+        
+        func checkOptionEquivalence<Option>(longOption: Option, setValue: Option.Value,
+                                            shortOption: NIOClientTCPBootstrap.Option) throws
+            where Option : ChannelOption, Option.Value : Equatable {
+            let longSetValue = try setAndGetOption(
+                option: longOption, { bs in
+                bs.channelOption(longOption, value: setValue) })
+            let shortSetValue = try setAndGetOption(
+                option: longOption, { bs in
+                    bs.channelOptions([shortOption])})
+            let unsetValue = try setAndGetOption(
+                option: longOption,
+                { $0 })
+            
+            XCTAssertEqual(longSetValue, shortSetValue)
+            XCTAssertNotEqual(longSetValue, unsetValue)
+        }
+        
+        try checkOptionEquivalence(longOption: ChannelOptions.socketOption(.reuseaddr),
+                                   setValue: 1,
+                                   shortOption: .allowImmediateEndpointAddressReuse)
+        try checkOptionEquivalence(longOption: ChannelOptions.allowRemoteHalfClosure,
+                                   setValue: true,
+                                   shortOption: .allowRemoteHalfClosure)
+    }
+    
     func testShorthandOptionsAreEquivalentDatagram() throws {
         func setAndGetOption<Option>(option: Option, _ applyOptions : (DatagramBootstrap) -> DatagramBootstrap) throws
             -> Option.Value where Option : ChannelOption {
