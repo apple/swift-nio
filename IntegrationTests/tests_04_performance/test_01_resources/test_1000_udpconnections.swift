1d134ccdcc9a02e3ec0c0b5b325c58b57362fe26
Peter Adams <63288215+PeterAdams-A@users.noreply.github.com>
2020-05-29 12:49:34 +0100

    Fine grained UDP allocation tests (#1534)
    Fine grained UDP allocation tests
    
    Motivation:
    
    The existing UDP allocation tests count allocations everywhere.
    
    Split out 3 tests to cover:
    
    Making connections.
    Making bootstraps.
    Transferring data.
    Modifications:
    
    Three new tests.
    
    Result:
    
    Easier to debug changes to allocation profiles in UDP.

---
+++ IntegrationTests/tests_04_performance/test_01_resources/test_1000_udpconnections.swift
@@ -0,0 +1,56 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2020 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import NIO
+
+fileprivate final class DoNothingHandler: ChannelInboundHandler {
+    public typealias InboundIn = ByteBuffer
+    public typealias OutboundOut = ByteBuffer
+}
+
+func run(identifier: String) {
+    let serverChannel = try! DatagramBootstrap(group: group)
+        .channelOption(ChannelOptions.socketOption(.so_reuseaddr), value: 1)
+        // Set the handlers that are applied to the bound channel
+        .channelInitializer { channel in
+            return channel.pipeline.addHandler(DoNothingHandler())
+        }
+        .bind(to: localhostPickPort).wait()
+    defer {
+        try! serverChannel.close().wait()
+    }
+
+    let remoteAddress = serverChannel.localAddress!
+    
+    let clientBootstrap = DatagramBootstrap(group: group)
+            .channelOption(ChannelOptions.socketOption(.so_reuseaddr), value: 1)
+
+    measure(identifier: identifier) {
+        let numberOfIterations = 1000
+        var buffer = ByteBufferAllocator().buffer(capacity: 1)
+        buffer.writeInteger(1, as: UInt8.self)
+        for _ in 0 ..< numberOfIterations {
+            let clientChannel = try! clientBootstrap.bind(to: localhostPickPort).wait()
+            defer {
+                try! clientChannel.close().wait()
+            }
+            
+            // Send a byte to make sure everything is really open.
+            let envelope = AddressedEnvelope<ByteBuffer>(remoteAddress: remoteAddress, data: buffer)
+            clientChannel.writeAndFlush(envelope, promise: nil)
+        }
+        return numberOfIterations
+    }
+}
+
