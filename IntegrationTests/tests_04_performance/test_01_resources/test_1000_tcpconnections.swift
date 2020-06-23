01e0e0cef4bd0bb5346a6c18d19583ae4911ea14
Johannes Weiss <johannesweiss@apple.com>
2020-06-23 13:11:43 +0100

    stablise 1000 TCP connections alloc test (#1566)
    Motivation:
    
    The 1000 TCP connections allocation counter test did way too many thread
    crosses and other things that allocated for unrelated reasons.
    
    Modifications:
    
    Remove those.
    
    Result:
    
    Much more stable

--- IntegrationTests/tests_04_performance/test_01_resources/test_1000_tcpconnections.swift
+++ IntegrationTests/tests_04_performance/test_01_resources/test_1000_tcpconnections.swift
@@ -17,38 +17,50 @@ import NIO
 fileprivate final class ReceiveAndCloseHandler: ChannelInboundHandler {
     public typealias InboundIn = ByteBuffer
     public typealias OutboundOut = ByteBuffer
-    
+
     func channelRead(context: ChannelHandlerContext, data: NIOAny) {
         let byteBuffer = self.unwrapInboundIn(data)
         precondition(byteBuffer.readableBytes == 1)
         context.channel.close(promise: nil)
     }
+
+    func errorCaught(context: ChannelHandlerContext, error: Error) {
+        fatalError("unexpected \(error)")
+    }
 }
 
 func run(identifier: String) {
+    let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+    defer {
+        try! group.syncShutdownGracefully()
+    }
+
     let serverChannel = try! ServerBootstrap(group: group)
             .serverChannelOption(ChannelOptions.socketOption(.so_reuseaddr), value: 1)
             .childChannelInitializer { channel in
                 channel.pipeline.addHandler(ReceiveAndCloseHandler())
-            }.bind(to: localhostPickPort).wait()
+            }.bind(host: "127.0.0.1", port: 0).wait()
     defer {
         try! serverChannel.close().wait()
     }
-    
+
     let clientBootstrap = ClientBootstrap(group: group)
-    
+
     measure(identifier: identifier) {
         let numberOfIterations = 1000
-        
+        let serverAddress = serverChannel.localAddress!
+        let buffer = ByteBuffer(integer: 1, as: UInt8.self)
+        let el = group.next()
+
         for _ in 0 ..< numberOfIterations {
-            let clientChannel = try! clientBootstrap.connect(to: serverChannel.localAddress!)
-                    .wait()
-            // Send a byte to make sure everything is really open.
-            var buffer = clientChannel.allocator.buffer(capacity: 1)
-            buffer.writeInteger(1, as: UInt8.self)
-            clientChannel.writeAndFlush(NIOAny(buffer), promise: nil)
-            // Wait for the close to come from the server side.
-            try! clientChannel.closeFuture.wait()
+            try! el.flatSubmit {
+                clientBootstrap.connect(to: serverAddress).flatMap { clientChannel in
+                    // Send a byte to make sure everything is really open.
+                    clientChannel.writeAndFlush(buffer).flatMap {
+                        clientChannel.closeFuture
+                    }
+                }
+            }.wait()
         }
         return numberOfIterations
     }
