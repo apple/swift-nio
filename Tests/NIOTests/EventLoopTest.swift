a7cd149273fcf0c7406366aeaabe17976badbecd
Michal A <dev@pokryfka.net>
2020-08-03 18:03:43 +0800

    Improved EventLoopGroupProvider test

--- Tests/NIOTests/EventLoopTest.swift
+++ Tests/NIOTests/EventLoopTest.swift
@@ -1282,20 +1282,15 @@ public final class EventLoopTest : XCTestCase {
 
     func testEventLoopGroupProvider() {
         let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-        let eventLoopGroup2 = MultiThreadedEventLoopGroup(numberOfThreads: 1)
         defer {
             XCTAssertNoThrow(try eventLoopGroup.syncShutdownGracefully())
-            XCTAssertNoThrow(try eventLoopGroup2.syncShutdownGracefully())
         }
 
         let provider = EventLoopGroupProvider.shared(eventLoopGroup)
 
-        if case EventLoopGroupProvider.shared(let sharedEventLoopGroup) = provider {
+        if case .shared(let sharedEventLoopGroup) = provider {
             XCTAssertTrue(sharedEventLoopGroup is MultiThreadedEventLoopGroup)
-            // this is sketchy but I dont think we can do better without changing MultiThreadedEventLoopGroup
-            // note that the description contains `myGroupID`
-            XCTAssertEqual(sharedEventLoopGroup.description, eventLoopGroup.description)
-            XCTAssertNotEqual(sharedEventLoopGroup.description, eventLoopGroup2.description)
+            XCTAssertTrue(sharedEventLoopGroup === eventLoopGroup)
         } else {
             XCTFail("Not the same")
         }
