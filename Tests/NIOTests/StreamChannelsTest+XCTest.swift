231928a2f7c52b15709e46a2a528f71970e5d2be
Joakim Hassila <joj@mac.com>
2021-04-06 07:03:09 +0200

    Reenable testHalfCloseOwnOutput for soundness test

--- Tests/NIOTests/StreamChannelsTest+XCTest.swift
+++ Tests/NIOTests/StreamChannelsTest+XCTest.swift
@@ -31,7 +31,7 @@ extension StreamChannelTest {
                 ("testSyncChannelOptions", testSyncChannelOptions),
                 ("testChannelReturnsNilForDefaultSyncOptionsImplementation", testChannelReturnsNilForDefaultSyncOptionsImplementation),
                 ("testWritabilityStartsTrueGoesFalseAndBackToTrue", testWritabilityStartsTrueGoesFalseAndBackToTrue),
-// FIXME:                ("testHalfCloseOwnOutput", testHalfCloseOwnOutput),
+                ("testHalfCloseOwnOutput", testHalfCloseOwnOutput),
                 ("testHalfCloseOwnInput", testHalfCloseOwnInput),
                 ("testDoubleShutdownInput", testDoubleShutdownInput),
                 ("testDoubleShutdownOutput", testDoubleShutdownOutput),
