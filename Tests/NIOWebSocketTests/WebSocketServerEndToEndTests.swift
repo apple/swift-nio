6acc2cf5d38e8cda243645aed3a74278a47b310b
Cory Benfield <lukasa@apple.com>
2021-08-05 08:19:37 +0100

    Move the Codecs to NIOCore
    Motivation:
    
    Our basic Channel Handlers don't need to be in NIO anymore: they aren't
    realistically tied to that code. So we can move them to NIOCore.
    
    Modifications:
    
    - Move Codecs to NIOCore
    - Move SingleStepByteToMessageDecoder to NIOCore.
    
    Result:
    
    Even more stuff in NIOCore

--- Tests/NIOWebSocketTests/WebSocketServerEndToEndTests.swift
+++ Tests/NIOWebSocketTests/WebSocketServerEndToEndTests.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the SwiftNIO open source project
 //
-// Copyright (c) 2017-2019 Apple Inc. and the SwiftNIO project authors
+// Copyright (c) 2017-2021 Apple Inc. and the SwiftNIO project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
@@ -13,7 +13,8 @@
 //===----------------------------------------------------------------------===//
 
 import XCTest
-@testable import NIO
+@testable import NIOCore
+import NIO
 import NIOHTTP1
 @testable import NIOWebSocket
 
