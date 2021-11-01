697503677d71bb0d3b29eb10df44730808d1b174
Adam Fowler <adamfowler71@gmail.com>
2021-11-01 09:29:04 +0000

    Conform EventLoopFuture/Promise to Sendable (#1982)
    * Conform EventLoopFuture/Promise to Sendable
    
    * Only conform to Sendable if concurrency is available
    
    * Add NIOSendable

--- Sources/NIOCore/EventLoopFuture.swift
+++ Sources/NIOCore/EventLoopFuture.swift
@@ -1570,3 +1570,15 @@ public struct _NIOEventLoopFutureIdentifier: Hashable {
         return UInt(bitPattern: ObjectIdentifier(future)) ^ 0xbf15ca5d
     }
 }
+
+// EventLoopPromise is a reference type, but by its very nature is Sendable.
+extension EventLoopPromise: NIOSendable { }
+
+#if swift(>=5.5) && canImport(_Concurrency)
+
+// EventLoopFuture is a reference type, but it is Sendable. However, we enforce
+// that by way of the guarantees of the EventLoop protocol, so the compiler cannot
+// check it.
+extension EventLoopFuture: @unchecked NIOSendable { }
+
+#endif
