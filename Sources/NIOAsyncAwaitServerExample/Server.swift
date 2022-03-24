2ddf737207980a0deb6f71674af888f38ffda977
Cory Benfield <lukasa@apple.com>
2022-03-24 08:12:30 +0000

    Use @main

--- Sources/NIOAsyncAwaitServerExample/Server.swift
+++ Sources/NIOAsyncAwaitServerExample/Server.swift
@@ -14,6 +14,21 @@
 import NIOCore
 import NIOPosix
 
+// The purpose of this example is to demonstrate our recommended pattern for structuring
+// NIO-using applications that want to take full advantage of async/await, while operating
+// as a server.
+//
+// This pattern is not necessarily entirely obvious. The goal is to ensure that the business
+// logic can fully take advantage of structured concurrency, ensuring that all the work tasks
+// have appropriate parent tasks that will be able to cancel and monitor them as needed. This
+// is not so difficult with a client, but with a server it requires substantial extra thought.
+// In addition, you need to bridge the async/await world with the NIO world, which can make
+// a somewhat difficult problem even more subtle.
+//
+// This example program implements a basic TCP echo server, with the business logic in
+// async/await code. The reason to keep the logic so simple is to allow us to focus on the
+// structure of the program, including the way it manages tasks.
+
 #if canImport(_Concurrency) && compiler(>=5.5.2)
 
 /// `Server` is the top-level object, responsible for running our
@@ -22,9 +37,15 @@ import NIOPosix
 /// This object owns the configuration and manages the setup and
 /// teardown of the server state.
 @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
+@main
 final class Server {
     private let host: String
     private let port: Int
+
+    static func main() async {
+        let server = Server(host: "localhost", port: 8765)
+        try! await server.run()
+    }
 
     init(host: String, port: Int) {
         self.host = host
@@ -171,5 +192,4 @@ fileprivate struct ChannelAndStream<InboundIn> {
     var channel: Channel
     var stream: NIOInboundChannelStream<InboundIn>
 }
-
 #endif
