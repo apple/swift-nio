3e8e32cb4d2d706cc0b04d17ec6781c0a6cc8069
Cory Benfield <lukasa@apple.com>
2022-04-23 07:03:31 +0100

    Make Future.get() cancellable.
    Motivation
    
    Currently async tasks that are waiting on NIO's EventLoopFuture will get
    stuck forever if that Task is cancelled. This inhibits the good
    behaviour of TaskGroup and other structured concurrency patterns that
    need to have all their subtasks exit before they can.
    
    In general users need to do more cleanup than simply cancelling a
    Future, however if they forget (or don't know what that cleanup is) it's
    still useful to be able to cancel an operation in this way.
    
    Modifications
    
    Add an extra intermediary Promise that we can fail.
    Add a cancellation handler that fails that Promise.
    
    Result
    
    EventLoopFuture.get() immediately fails on cancellation.

--- Sources/NIOCore/AsyncAwaitSupport.swift
+++ Sources/NIOCore/AsyncAwaitSupport.swift
@@ -28,16 +28,26 @@ extension EventLoopFuture {
     @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
     @inlinable
     public func get() async throws -> Value {
-        return try await withUnsafeThrowingContinuation { cont in
-            self.whenComplete { result in
-                switch result {
-                case .success(let value):
-                    cont.resume(returning: value)
-                case .failure(let error):
-                    cont.resume(throwing: error)
+        // We need an extra promise in order to make this cancellable.
+        let promise = self.eventLoop.makePromise(of: Value.self)
+        self.cascade(to: promise)
+
+        return try await withTaskCancellationHandler {
+            return try await withUnsafeThrowingContinuation { cont in
+                promise.futureResult.whenComplete { result in
+                    switch result {
+                    case .success(let value):
+                        cont.resume(returning: value)
+                    case .failure(let error):
+                        cont.resume(throwing: error)
+                    }
                 }
             }
+        } onCancel: {
+            promise.fail(CancellationError())
         }
+
+
     }
 }
 
