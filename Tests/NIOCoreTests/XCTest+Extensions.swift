5c6e2fa7660c1f0f7ab21b043ac5a4f6b8be7740
Si Beaumont <beaumont@apple.com>
2022-05-04 11:51:38 +0100

    fixup: Port some test utils to not use NIOPosix
    Signed-off-by: Si Beaumont <beaumont@apple.com>

--- Tests/NIOCoreTests/XCTest+Extensions.swift
+++ Tests/NIOCoreTests/XCTest+Extensions.swift
@@ -14,6 +14,7 @@
 
 import XCTest
 import NIOCore
+import struct System.FileDescriptor
 
 func assert(_ condition: @autoclosure () -> Bool, within time: TimeAmount, testInterval: TimeAmount? = nil, _ message: String = "condition not satisfied in time", file: StaticString = #file, line: UInt = #line) {
     let testInterval = testInterval ?? TimeAmount.nanoseconds(time.nanoseconds / 5)
@@ -42,7 +43,7 @@ func assertNoThrowWithValue<T>(_ body: @autoclosure () throws -> T, defaultValue
     }
 }
 
-func withTemporaryFile<T>(content: String? = nil, _ body: (NIOCore.NIOFileHandle, String) throws -> T) rethrows -> T {
+func withTemporaryFile<T>(content: String? = nil, _ body: (NIOCore.NIOFileHandle, String) throws -> T) throws -> T {
     let (fd, path) = openTemporaryFile()
     let fileHandle = NIOFileHandle(descriptor: fd)
     defer {
@@ -50,22 +51,42 @@ func withTemporaryFile<T>(content: String? = nil, _ body: (NIOCore.NIOFileHandle
         XCTAssertEqual(0, unlink(path))
     }
     if let content = content {
-        try Array(content.utf8).withUnsafeBufferPointer { ptr in
-            var toWrite = ptr.count
-            var start = ptr.baseAddress!
-            while toWrite > 0 {
-                let res = try Posix.write(descriptor: fd, pointer: start, size: toWrite)
-                switch res {
-                case .processed(let written):
-                    toWrite -= written
-                    start = start + written
-                case .wouldBlock:
-                    XCTFail("unexpectedly got .wouldBlock from a file")
-                    continue
-                }
-            }
-            XCTAssertEqual(0, lseek(fd, 0, SEEK_SET))
-        }
+        let fileDesciptor = FileDescriptor(rawValue: fd)
+        try fileDesciptor.writeAll(content.utf8)
+        XCTAssertEqual(try fileDesciptor.seek(offset: 0, from: .start), 0)
     }
     return try body(fileHandle, path)
+}
+
+fileprivate var temporaryDirectory: String {
+    get {
+#if targetEnvironment(simulator)
+        // Simulator temp directories are so long (and contain the user name) that they're not usable
+        // for UNIX Domain Socket paths (which are limited to 103 bytes).
+        return "/tmp"
+#else
+#if os(Linux)
+        return "/tmp"
+#else
+        if #available(macOS 10.12, iOS 10, tvOS 10, watchOS 3, *) {
+            return FileManager.default.temporaryDirectory.path
+        } else {
+            return "/tmp"
+        }
+#endif // os
+#endif // targetEnvironment
+    }
+}
+
+func openTemporaryFile() -> (CInt, String) {
+    let template = "\(temporaryDirectory)/nio_XXXXXX"
+    var templateBytes = template.utf8 + [0]
+    let templateBytesCount = templateBytes.count
+    let fd = templateBytes.withUnsafeMutableBufferPointer { ptr in
+        ptr.baseAddress!.withMemoryRebound(to: Int8.self, capacity: templateBytesCount) { (ptr: UnsafeMutablePointer<Int8>) in
+            return mkstemp(ptr)
+        }
+    }
+    templateBytes.removeLast()
+    return (fd, String(decoding: templateBytes, as: Unicode.UTF8.self))
 }
