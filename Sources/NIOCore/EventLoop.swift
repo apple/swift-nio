470bba6c620206fa956f00cec78105acd133c61a
Si Beaumont <beaumont@apple.com>
2022-05-16 22:30:53 +0100

    wip: Use clock_gettime for NIODeadline.now()
    Signed-off-by: Si Beaumont <beaumont@apple.com>

--- Sources/NIOCore/EventLoop.swift
+++ Sources/NIOCore/EventLoop.swift
@@ -14,6 +14,7 @@
 
 import NIOConcurrencyHelpers
 import Dispatch
+import CNIOLinux
 
 /// Returned once a task was scheduled on the `EventLoop` for later execution.
 ///
@@ -478,8 +479,22 @@ public struct NIODeadline: Equatable, Hashable, NIOSendable {
         self._uptimeNanoseconds = nanoseconds
     }
 
+
+//#if os(Linux)
+    @inline(__always)
+    private static func timeNow() -> UInt64 {
+        if #available(macOS 10.12, *) {
+            var ts = timespec()
+            clock_gettime(CLOCK_MONOTONIC, &ts)
+            return UInt64(ts.tv_sec) &* 1_000_000_000 &+ UInt64(ts.tv_nsec)
+        } else {
+            return DispatchTime.now().uptimeNanoseconds
+        }
+    }
+//#endif
+
     public static func now() -> NIODeadline {
-        return NIODeadline.uptimeNanoseconds(DispatchTime.now().uptimeNanoseconds)
+        return NIODeadline.uptimeNanoseconds(timeNow())
     }
 
     public static func uptimeNanoseconds(_ nanoseconds: UInt64) -> NIODeadline {
