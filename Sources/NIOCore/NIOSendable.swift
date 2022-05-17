3dcb0f5e9b65eea36689833eae08334d6f155034
David Nadoba <d_nadoba@apple.com>
2022-05-17 09:58:11 +0200

    Introduce internal `UnsafeTransfer` type (#2115)
    Motivation:
    
    Soon we will need to silence some `Sendable` warnings. 
    The recommended way is to use a wrapper type that is unconditionally 
    `Sendable`. 
    
    https://github.com/apple/swift-evolution/blob/main/proposals/0302-concurrent-value-and-concurrent-closures.md#adaptor-types-for-legacy-codebases
    
    Modifications:
    
    - add internal `UnsafeTransfer` type
    
    Result:
    
    We can silence `Sendable` warnings and errors by using `UnsafeTransfer` to pass non-`Sendable` values around.

--- Sources/NIOCore/NIOSendable.swift
+++ Sources/NIOCore/NIOSendable.swift
@@ -23,3 +23,22 @@ public typealias NIOSendable = Any
 #else
 public protocol NIOPreconcurrencySendable {}
 #endif
+
+#if swift(>=5.5) && canImport(_Concurrency)
+/// ``UnsafeTransfer`` can be used to make non-`Sendable` values `Sendable`.
+/// As the name implies, the usage of this is unsafe because it disables the sendable checking of the compiler.
+/// It can be used similar to `@unsafe Sendable` but for values instead of types.
+@usableFromInline
+struct UnsafeTransfer<Wrapped>: @unchecked Sendable {
+    @usableFromInline
+    var wrappedValue: Wrapped
+    
+    @inlinable
+    init(_ wrappedValue: Wrapped) {
+        self.wrappedValue = wrappedValue
+    }
+}
+
+extension UnsafeTransfer: Equatable where Wrapped: Equatable {}
+extension UnsafeTransfer: Hashable where Wrapped: Hashable {}
+#endif
