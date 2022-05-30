78c97a3cda3d5e311524ae83b6b81cd21518d548
Saleem Abdulrasool <compnerd@compnerd.org>
2022-05-30 10:15:24 -0700

    NIOCore: add missing import for `IO` (#2129)
    Import `ucrt` for access to `strerror` on Windows.

--- Sources/NIOCore/IO.swift
+++ Sources/NIOCore/IO.swift
@@ -13,6 +13,7 @@
 //===----------------------------------------------------------------------===//
 
 #if os(Windows)
+import ucrt
 import typealias WinSDK.DWORD
 #elseif os(Linux) || os(Android)
 import Glibc
