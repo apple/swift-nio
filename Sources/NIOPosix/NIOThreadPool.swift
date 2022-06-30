427d358d037c9a29ad51869d4d4ae04403e7c029
David Nadoba <d_nadoba@apple.com>
2022-06-30 17:21:50 +0200

    Adopt `Sendable` for types in `NIOPosix` (#2208)

--- Sources/NIOPosix/NIOThreadPool.swift
+++ Sources/NIOPosix/NIOThreadPool.swift
@@ -42,7 +42,7 @@ public enum NIOThreadPoolError {
 public final class NIOThreadPool {
 
     /// The state of the `WorkItem`.
-    public enum WorkItemState {
+    public enum WorkItemState: NIOSendable {
         /// The `WorkItem` is active now and in process by the `NIOThreadPool`.
         case active
         /// The `WorkItem` was cancelled and will not be processed by the `NIOThreadPool`.
@@ -212,6 +212,10 @@ public final class NIOThreadPool {
         }
     }
 }
+
+#if swift(>=5.5) && canImport(_Concurrency)
+extension NIOThreadPool: @unchecked Sendable {}
+#endif
 
 extension NIOThreadPool {
     
