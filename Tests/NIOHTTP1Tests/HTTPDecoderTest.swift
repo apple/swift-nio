edc2158df3e85a1011b02999d0198a59f23d2d21
Colton Loewen <loewencjay@outlook.com>
2022-09-01 21:14:11 -0600

    Added ICY response test case

--- Tests/NIOHTTP1Tests/HTTPDecoderTest.swift
+++ Tests/NIOHTTP1Tests/HTTPDecoderTest.swift
@@ -177,6 +177,22 @@ class HTTPDecoderTest: XCTestCase {
         XCTAssertNoThrow(try channel.writeInbound(buffer))
         XCTAssertNoThrow(try channel.finish())
     }
+    
+    func testToleratesIcyResponse() throws {
+        XCTAssertNoThrow(try channel.pipeline.addHandler(HTTPRequestEncoder()).wait())
+        XCTAssertNoThrow(try channel.pipeline.addHandler(ByteToMessageHandler(HTTPResponseDecoder())).wait())
+
+        // We need to prime the decoder by seeing a GET request.
+        try channel.writeOutbound(HTTPClientRequestPart.head(HTTPRequestHead(version: .http2, method: .GET, uri: "/")))
+
+        // We tolerate SOURCETABLE responses due to NTRIP revision 1.
+        // says that these should be treated like HTTP/1.1 by our users.
+        var buffer = channel.allocator.buffer(capacity: 64)
+        buffer.writeStaticString("ICY 200 OK\r\n\r\n")
+
+        XCTAssertNoThrow(try channel.writeInbound(buffer))
+        XCTAssertNoThrow(try channel.finish())
+    }
 
     func testCorrectlyMaintainIndicesWhenDiscardReadBytes() throws {
         class Receiver: ChannelInboundHandler {
