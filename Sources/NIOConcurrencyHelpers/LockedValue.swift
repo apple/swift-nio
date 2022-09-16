e457aa5b9ded16c4295f524804da40d0e74d6a93
Cory Benfield <lukasa@apple.com>
2022-09-16 16:42:55 +0100

    Update Sources/NIOConcurrencyHelpers/LockedValue.swift

--- Sources/NIOConcurrencyHelpers/LockedValue.swift
+++ Sources/NIOConcurrencyHelpers/LockedValue.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the SwiftNIO open source project
 //
-// Copyright (c) YEARS Apple Inc. and the SwiftNIO project authors
+// Copyright (c) 2022 Apple Inc. and the SwiftNIO project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
