apple/swift-nio

commit 4ed8e1e22833c705ff002bc46d8fecca0355e786

rename class Lock to struct NIOLock (#2266)

Sources/NIOConcurrencyHelpers/NIOLock.swift
@@ -1,0 +1,148 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2017-2022 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+#if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
+import Darwin
+#elseif os(Windows)
+import ucrt
+import WinSDK
+#else
+import Glibc
+#endif
+
+/// A threading lock based on `libpthread` instead of `libdispatch`.
+///
+/// - note: ``NIOLock`` has reference semantics.
+///
+/// This object provides a lock on top of a single `pthread_mutex_t`. This kind
+/// of lock is safe to use with `libpthread`-based threading models, such as the
+/// one used by NIO. On Windows, the lock is based on the substantially similar
+/// `SRWLOCK` type.
+public struct NIOLock {
+    @usableFromInline
+    internal let _storage: _Storage
+
+#if os(Windows)
+    @usableFromInline
+    internal typealias LockPrimitive = SRWLOCK
+#else
+    @usableFromInline
+    internal typealias LockPrimitive = pthread_mutex_t
+#endif
+    
+    @usableFromInline
+    internal final class _Storage {
+        // TODO: We should tail-allocate the pthread_t/SRWLock.
+        @usableFromInline
+        internal let mutex: UnsafeMutablePointer<LockPrimitive> =
+        UnsafeMutablePointer.allocate(capacity: 1)
+
+        /// Create a new lock.
+        internal init() {
+#if os(Windows)
+            InitializeSRWLock(self.mutex)
+#else
+            var attr = pthread_mutexattr_t()
+            pthread_mutexattr_init(&attr)
+            debugOnly {
+                pthread_mutexattr_settype(&attr, .init(PTHREAD_MUTEX_ERRORCHECK))
+            }
+            
+            let err = pthread_mutex_init(self.mutex, &attr)
+            precondition(err == 0, "\(#function) failed in pthread_mutex with error \(err)")
+#endif
+        }
+        
+        internal func lock() {
+#if os(Windows)
+            AcquireSRWLockExclusive(self.mutex)
+#else
+            let err = pthread_mutex_lock(self.mutex)
+            precondition(err == 0, "\(#function) failed in pthread_mutex with error \(err)")
+#endif
+        }
+        
+        internal func unlock() {
+#if os(Windows)
+            ReleaseSRWLockExclusive(self._storage.mutex)
+#else
+            let err = pthread_mutex_unlock(self.mutex)
+            precondition(err == 0, "\(#function) failed in pthread_mutex with error \(err)")
+#endif
+        }
+
+        internal func withLockPrimitive<T>(_ body: (UnsafeMutablePointer<LockPrimitive>) throws -> T) rethrows -> T {
+            return try body(self.mutex)
+        }
+        
+        deinit {
+#if os(Windows)
+            // SRWLOCK does not need to be free'd
+#else
+            let err = pthread_mutex_destroy(self.mutex)
+            precondition(err == 0, "\(#function) failed in pthread_mutex with error \(err)")
+#endif
+            mutex.deallocate()
+        }
+    }
+    
+    /// Create a new lock.
+    public init() {
+        self._storage = _Storage()
+    }
+
+    /// Acquire the lock.
+    ///
+    /// Whenever possible, consider using `withLock` instead of this method and
+    /// `unlock`, to simplify lock handling.
+    public func lock() {
+        self._storage.lock()
+    }
+
+    /// Release the lock.
+    ///
+    /// Whenever possible, consider using `withLock` instead of this method and
+    /// `lock`, to simplify lock handling.
+    public func unlock() {
+        self._storage.unlock()
+    }
+
+    internal func withLockPrimitive<T>(_ body: (UnsafeMutablePointer<LockPrimitive>) throws -> T) rethrows -> T {
+        return try self._storage.withLockPrimitive(body)
+    }
+}
+
+extension NIOLock {
+    /// Acquire the lock for the duration of the given block.
+    ///
+    /// This convenience method should be preferred to `lock` and `unlock` in
+    /// most situations, as it ensures that the lock will be released regardless
+    /// of how `body` exits.
+    ///
+    /// - Parameter body: The block to execute while holding the lock.
+    /// - Returns: The value returned by the block.
+    @inlinable
+    public func withLock<T>(_ body: () throws -> T) rethrows -> T {
+        self.lock()
+        defer {
+            self.unlock()
+        }
+        return try body()
+    }
+}
+
+#if compiler(>=5.5) && canImport(_Concurrency)
+extension NIOLock: Sendable {}
+extension NIOLock._Storage: Sendable {}
+#endif

Sources/NIOConcurrencyHelpers/lock.swift
@@ -27,6 +27,7 @@ import Glibc
 /// of lock is safe to use with `libpthread`-based threading models, such as the
 /// one used by NIO. On Windows, the lock is based on the substantially similar
 /// `SRWLOCK` type.
+@available(*, deprecated, renamed: "NIOLock")
 public final class Lock {
 #if os(Windows)
     fileprivate let mutex: UnsafeMutablePointer<SRWLOCK> =
@@ -89,6 +90,7 @@ public final class Lock {
     }
 }
 
+@available(*, deprecated, renamed: "NIOLock")
 extension Lock {
     /// Acquire the lock for the duration of the given block.
     ///
@@ -120,7 +122,7 @@ extension Lock {
 /// until the state variable is set to a specific value to acquire the lock.
 public final class ConditionLock<T: Equatable> {
     private var _value: T
-    private let mutex: Lock
+    private let mutex: NIOLock
 #if os(Windows)
     private let cond: UnsafeMutablePointer<CONDITION_VARIABLE> =
         UnsafeMutablePointer.allocate(capacity: 1)
@@ -134,7 +136,7 @@ public final class ConditionLock<T: Equatable> {
     /// - Parameter value: The initial value to give the state variable.
     public init(value: T) {
         self._value = value
-        self.mutex = Lock()
+        self.mutex = NIOLock()
 #if os(Windows)
         InitializeConditionVariable(self.cond)
 #else
@@ -186,13 +188,15 @@ public final class ConditionLock<T: Equatable> {
             if self._value == wantedValue {
                 break
             }
+            self.mutex.withLockPrimitive { mutex in
 #if os(Windows)
-            let result = SleepConditionVariableSRW(self.cond, self.mutex.mutex, INFINITE, 0)
-            precondition(result, "\(#function) failed in SleepConditionVariableSRW with error \(GetLastError())")
+                let result = SleepConditionVariableSRW(self.cond, mutex, INFINITE, 0)
+                precondition(result, "\(#function) failed in SleepConditionVariableSRW with error \(GetLastError())")
 #else
-            let err = pthread_cond_wait(self.cond, self.mutex.mutex)
-            precondition(err == 0, "\(#function) failed in pthread_cond with error \(err)")
+                let err = pthread_cond_wait(self.cond, mutex)
+                precondition(err == 0, "\(#function) failed in pthread_cond with error \(err)")
 #endif
+            }
         }
     }
 
@@ -244,18 +248,20 @@ public final class ConditionLock<T: Equatable> {
                                   tv_nsec: Int(allNSecs % nsecPerSec))
         assert(timeoutAbs.tv_nsec >= 0 && timeoutAbs.tv_nsec < Int(nsecPerSec))
         assert(timeoutAbs.tv_sec >= curTime.tv_sec)
-        while true {
-            if self._value == wantedValue {
-                return true
-            }
-            switch pthread_cond_timedwait(self.cond, self.mutex.mutex, &timeoutAbs) {
-            case 0:
-                continue
-            case ETIMEDOUT:
-                self.unlock()
-                return false
-            case let e:
-                fatalError("caught error \(e) when calling pthread_cond_timedwait")
+        return self.mutex.withLockPrimitive { mutex -> Bool in
+            while true {
+                if self._value == wantedValue {
+                    return true
+                }
+                switch pthread_cond_timedwait(self.cond, mutex, &timeoutAbs) {
+                case 0:
+                    continue
+                case ETIMEDOUT:
+                    self.unlock()
+                    return false
+                case let e:
+                    fatalError("caught error \(e) when calling pthread_cond_timedwait")
+                }
             }
         }
 #endif
@@ -288,6 +294,7 @@ internal func debugOnly(_ body: () -> Void) {
 }
 
 #if compiler(>=5.5) && canImport(_Concurrency)
+@available(*, deprecated, renamed: "NIOLock")
 extension Lock: Sendable {
 
 }

IntegrationTests/tests_05_assertions/test_01_syscall_wrapper_fast.sh
@@ -26,14 +26,14 @@ elif [[ "$(uname -s)" == "Linux" ]]; then
     swift_binary=$(which swiftc)
 fi
 
-cp "$here/../../Sources/NIOConcurrencyHelpers/lock.swift" "$tmp"
+cp "$here/../../Sources/NIOConcurrencyHelpers/"{lock,NIOLock}.swift "$tmp"
 cat > "$tmp/main.swift" <<"EOF"
-let l = Lock()
+let l = NIOLock()
 l.lock()
 l.lock()
 EOF
 
-"$swift_binary" -o "$tmp/test" "$tmp/main.swift" "$tmp/lock.swift"
+"$swift_binary" -o "$tmp/test" "$tmp/main.swift" "$tmp/"{lock,NIOLock}.swift
 if "$tmp/test"; then
     fail "should have crashed"
 else

Sources/NIOConcurrencyHelpers/LockedValue.swift
@@ -25,7 +25,7 @@ public struct NIOLockedValueBox<Value> {
     @usableFromInline
     internal final class _Storage {
         @usableFromInline
-        internal let _lock = Lock()
+        internal let _lock = NIOLock()
         
         @usableFromInline
         internal var _value: Value

Sources/NIOCore/AsyncSequences/NIOAsyncWriter.swift
@@ -395,7 +395,7 @@ extension NIOAsyncWriter {
 
         /// The lock that protects our state.
         @usableFromInline
-        /* private */ internal let _lock = Lock()
+        /* private */ internal let _lock = NIOLock()
         /// The counter used to assign an ID to all our yields.
         @usableFromInline
         /* private */ internal let _yieldIDGenerator = YieldIDGenerator()

Sources/NIOCore/AsyncSequences/NIOThrowingAsyncSequenceProducer.swift
@@ -304,7 +304,7 @@ extension NIOThrowingAsyncSequenceProducer {
     /* fileprivate */ internal final class Storage: @unchecked Sendable {
         /// The lock that protects our state.
         @usableFromInline
-        /* private */ internal let _lock = Lock()
+        /* private */ internal let _lock = NIOLock()
         /// The state machine.
         @usableFromInline
         /* private */ internal var _stateMachine: StateMachine

Sources/NIOCore/EventLoop.swift
@@ -1186,7 +1186,7 @@ extension EventLoopGroup {
     public func syncShutdownGracefully() throws {
         self._preconditionSafeToSyncShutdown(file: #file, line: #line)
 
-        let errorStorageLock = Lock()
+        let errorStorageLock = NIOLock()
         var errorStorage: Error? = nil
         let continuation = DispatchWorkItem {}
         self.shutdownGracefully { error in

Sources/NIOEmbedded/AsyncTestingChannel.swift
@@ -194,7 +194,7 @@ public final class NIOAsyncTestingChannel: Channel {
     /*private but usableFromInline */ var channelcore: EmbeddedChannelCore!
 
     /// Guards any of the getters/setters that can be accessed from any thread.
-    private let stateLock: Lock = Lock()
+    private let stateLock: NIOLock = NIOLock()
 
     // Guarded by `stateLock`
     private var _isWritable: Bool = true
@@ -223,7 +223,7 @@ public final class NIOAsyncTestingChannel: Channel {
             return self.stateLock.withLock { self._isWritable }
         }
         set {
-            self.stateLock.withLockVoid {
+            self.stateLock.withLock { () -> Void in
                 self._isWritable = newValue
             }
         }
@@ -235,7 +235,7 @@ public final class NIOAsyncTestingChannel: Channel {
             return self.stateLock.withLock { self._localAddress }
         }
         set {
-            self.stateLock.withLockVoid {
+            self.stateLock.withLock { () -> Void in
                 self._localAddress = newValue
             }
         }
@@ -247,7 +247,7 @@ public final class NIOAsyncTestingChannel: Channel {
             return self.stateLock.withLock { self._remoteAddress }
         }
         set {
-            self.stateLock.withLockVoid {
+            self.stateLock.withLock { () -> Void in
                 self._remoteAddress = newValue
             }
         }

commit f9cb23a2d88bfeb4a5fd9ecb4aedd65c764bbba4

Fixed compile errors on windows
Compile error seems to be caused by https://github.com/apple/swift-nio/pull/2266/

Sources/NIOConcurrencyHelpers/NIOLock.swift
@@ -75,7 +75,7 @@ public struct NIOLock {
         
         internal func unlock() {
 #if os(Windows)
-            ReleaseSRWLockExclusive(self._storage.mutex)
+            ReleaseSRWLockExclusive(self.mutex)
 #else
             let err = pthread_mutex_unlock(self.mutex)
             precondition(err == 0, "\(#function) failed in pthread_mutex with error \(err)")

Sources/NIOConcurrencyHelpers/lock.swift
@@ -221,7 +221,7 @@ public final class ConditionLock<T: Equatable> {
             }
 
             let dwWaitStart = timeGetTime()
-            if !SleepConditionVariableSRW(self.cond, self.mutex.mutex,
+            if !SleepConditionVariableSRW(self.cond, self.mutex._storage.mutex,
                                           dwMilliseconds, 0) {
                 let dwError = GetLastError()
                 if (dwError == ERROR_TIMEOUT) {