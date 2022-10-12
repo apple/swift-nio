983ace6874ce0c492c21d7227a22e76256ac1a51
carolinacass <67160898+carolinacass@users.noreply.github.com>
2022-10-12 16:58:01 +0100

    Fix nightly builds
        Swift 5.8 has recently changed the name of the function assign to now be called update. Now, they call update like seen in this proposal:
        https://github.com/apple/swift-evolution/blob/main/proposals/0370-pointer-family-initialization-improvements.md

--- Tests/NIOPosixTests/SocketAddressTest.swift
+++ Tests/NIOPosixTests/SocketAddressTest.swift
@@ -575,7 +575,6 @@ class SocketAddressTest: XCTestCase {
     }
 }
 
-//we don't want to overide method'
 #if swift(<5.8)
 extension UnsafeMutablePointer {
     func update(from source: UnsafePointer<Pointee>, count: Int) {
