5b2dfd6d9fa53faa412fb006f01efebe59af446b
David Nadoba <d_nadoba@apple.com>
2022-11-23 22:46:28 +0100

    Add `IPv4Header` and tests for `.ip_hdrincl`

---
+++ Tests/NIOPosixTests/IPv4Header.swift
@@ -0,0 +1,277 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2022 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import NIOCore
+
+struct IPv4Address: Hashable {
+    var rawValue: UInt32
+}
+
+extension IPv4Address {
+    init(_ v1: UInt8, _ v2: UInt8, _ v3: UInt8, _ v4: UInt8) {
+        rawValue = UInt32(v1) << 24 | UInt32(v2) << 16 | UInt32(v3) << 8 | UInt32(v4)
+    }
+}
+
+extension IPv4Address: CustomStringConvertible {
+    var description: String {
+        let v1 = rawValue >> 24
+        let v2 = rawValue >> 16 & 0b1111_1111
+        let v3 = rawValue >> 8 & 0b1111_1111
+        let v4 = rawValue & 0b1111_1111
+        return "\(v1).\(v2).\(v3).\(v4)"
+    }
+}
+
+struct IPv4Header: Hashable {
+    static let size: Int = 20
+    
+    private var versionAndIhl: UInt8
+    var version: UInt8 {
+        get {
+            versionAndIhl >> 4
+        }
+        set {
+            precondition(newValue & 0b1111_0000 == 0)
+            versionAndIhl = newValue << 4 | (0b0000_1111 & versionAndIhl)
+            assert(newValue == version, "\(newValue) != \(version) \(versionAndIhl)")
+        }
+    }
+    var internetHeaderLength: UInt8 {
+        get {
+            versionAndIhl & 0b0000_1111
+        }
+        set {
+            precondition(newValue & 0b1111_0000 == 0)
+            versionAndIhl = newValue | (0b1111_0000 & versionAndIhl)
+            assert(newValue == internetHeaderLength)
+        }
+    }
+    private var dscpAndEcn: UInt8
+    var differentiatedServicesCodePoint: UInt8 {
+        get {
+            dscpAndEcn >> 2
+        }
+        set {
+            precondition(newValue & 0b0000_0011 == 0)
+            dscpAndEcn = newValue << 2 | (0b0000_0011 & dscpAndEcn)
+            assert(newValue == differentiatedServicesCodePoint)
+        }
+    }
+    var explicitCongestionNotification: UInt8 {
+        get {
+            dscpAndEcn & 0b0000_0011
+        }
+        set {
+            precondition(newValue & 0b0000_0011 == 0)
+            dscpAndEcn = newValue | (0b1111_1100 & dscpAndEcn)
+            assert(newValue == explicitCongestionNotification)
+        }
+    }
+    var totalLength: UInt16
+    var identification: UInt16
+    private var flagsAndFragmentOffset: UInt16
+    var flags: UInt8 {
+        get {
+            UInt8(flagsAndFragmentOffset >> 13)
+        }
+        set {
+            precondition(newValue & 0b0000_0111 == 0)
+            flagsAndFragmentOffset = UInt16(newValue) << 13 | (0b0001_1111_1111_1111 & flagsAndFragmentOffset)
+            assert(newValue == flags)
+        }
+    }
+    var fragmentOffset: UInt16 {
+        get {
+            flagsAndFragmentOffset & 0b0001_1111_1111_1111
+        }
+        set {
+            precondition(newValue & 0b1110_0000_0000_0000 == 0)
+            flagsAndFragmentOffset = newValue | (0b1110_0000_0000_0000 & flagsAndFragmentOffset)
+            assert(newValue == fragmentOffset)
+        }
+    }
+    var timeToLive: UInt8
+    var `protocol`: NIOIPProtocol
+    var headerChecksum: UInt16
+    var sourceIpAddress: IPv4Address
+    var destinationIpAddress: IPv4Address
+    
+    init?(buffer: inout ByteBuffer) {
+        #if canImport(Darwin)
+        guard
+            let versionAndIhl: UInt8 = buffer.readInteger(),
+            let dscpAndEcn: UInt8 = buffer.readInteger(),
+            // On BSD, the total length is in host byte order
+            let totalLength: UInt16 = buffer.readInteger(endianness: .host),
+            let identification: UInt16 = buffer.readInteger(),
+            // fragmentOffset is in host byte order as well but it is always zero in our tests
+            // and fragmentOffset is 13 bits in size so we can't just use readInteger(endianness: .host)
+            let flagsAndFragmentOffset: UInt16 = buffer.readInteger(),
+            let timeToLive: UInt8 = buffer.readInteger(),
+            let `protocol`: UInt8 = buffer.readInteger(),
+            let headerChecksum: UInt16 = buffer.readInteger(),
+            let sourceIpAddress: UInt32 = buffer.readInteger(),
+            let destinationIpAddress: UInt32 = buffer.readInteger()
+        else { return nil }
+        #elseif os(Linux)
+        guard let (
+            versionAndIhl,
+            dscpAndEcn,
+            totalLength,
+            identification,
+            flagsAndFragmentOffset,
+            timeToLive,
+            `protocol`,
+            headerChecksum,
+            sourceIpAddress,
+            destinationIpAddress
+        ) = buffer.readMultipleIntegers(as: (
+            UInt8,
+            UInt8,
+            UInt16,
+            UInt16,
+            UInt16,
+            UInt8,
+            UInt8,
+            UInt16,
+            UInt32,
+            UInt32
+        ).self) else { return nil }
+        #endif
+        self.versionAndIhl = versionAndIhl
+        self.dscpAndEcn = dscpAndEcn
+        self.totalLength = totalLength
+        self.identification = identification
+        self.flagsAndFragmentOffset = flagsAndFragmentOffset
+        self.timeToLive = timeToLive
+        self.`protocol` = .init(rawValue: `protocol`)
+        self.headerChecksum = headerChecksum
+        self.sourceIpAddress = .init(rawValue: sourceIpAddress)
+        self.destinationIpAddress = .init(rawValue: destinationIpAddress)
+    }
+    init() {
+        self.versionAndIhl = 0
+        self.dscpAndEcn = 0
+        self.totalLength = 0
+        self.identification = 0
+        self.flagsAndFragmentOffset = 0
+        self.timeToLive = 0
+        self.`protocol` = .init(rawValue: 0)
+        self.headerChecksum = 0
+        self.sourceIpAddress = .init(rawValue: 0)
+        self.destinationIpAddress = .init(rawValue: 0)
+    }
+    
+    mutating func setChecksum() {
+        self.headerChecksum = ~[
+            UInt16(versionAndIhl) << 8 | UInt16(dscpAndEcn),
+            totalLength,
+            identification,
+            flagsAndFragmentOffset,
+            UInt16(timeToLive) << 8 | UInt16(`protocol`.rawValue),
+            UInt16(sourceIpAddress.rawValue >> 16),
+            UInt16(sourceIpAddress.rawValue & 0b0000_0000_0000_0000_1111_1111_1111_1111),
+            UInt16(destinationIpAddress.rawValue >> 16),
+            UInt16(destinationIpAddress.rawValue & 0b0000_0000_0000_0000_1111_1111_1111_1111),
+        ].reduce(UInt16(0), onesComplementAdd)
+        
+        assert(isValidChecksum())
+    }
+    
+    func isValidChecksum() -> Bool {
+        let sum = ~[
+            UInt16(versionAndIhl) << 8 | UInt16(dscpAndEcn),
+            totalLength,
+            identification,
+            flagsAndFragmentOffset,
+            UInt16(timeToLive) << 8 | UInt16(`protocol`.rawValue),
+            headerChecksum,
+            UInt16(sourceIpAddress.rawValue >> 16),
+            UInt16(sourceIpAddress.rawValue & 0b0000_0000_0000_0000_1111_1111_1111_1111),
+            UInt16(destinationIpAddress.rawValue >> 16),
+            UInt16(destinationIpAddress.rawValue & 0b0000_0000_0000_0000_1111_1111_1111_1111),
+        ].reduce(UInt16(0), onesComplementAdd)
+        return sum == 0
+    }
+    
+    func write(to buffer: inout ByteBuffer) {
+        assert({
+            var buffer = ByteBuffer()
+            self._write(to: &buffer)
+            let newValue = Self(buffer: &buffer)
+            return self == newValue
+        }())
+        self._write(to: &buffer)
+    }
+    
+    private func _write(to buffer: inout ByteBuffer) {
+        #if canImport(Darwin)
+        buffer.writeInteger(versionAndIhl)
+        buffer.writeInteger(dscpAndEcn)
+        // On BSD, the total length needs to be in host byte order
+        buffer.writeInteger(totalLength, endianness: .host)
+        buffer.writeInteger(identification)
+        // fragmentOffset needs to be in host byte order as well but it is always zero in our tests
+        // and fragmentOffset is 13 bits in size so we can't just use writeInteger(endianness: .host)
+        buffer.writeInteger(flagsAndFragmentOffset)
+        buffer.writeInteger(timeToLive)
+        buffer.writeInteger(`protocol`.rawValue)
+        buffer.writeInteger(headerChecksum)
+        buffer.writeInteger(sourceIpAddress.rawValue)
+        buffer.writeInteger(destinationIpAddress.rawValue)
+        #elseif os(Linux)
+        buffer.writeMultipleIntegers(
+            versionAndIhl,
+            dscpAndEcn,
+            totalLength,
+            identification,
+            flagsAndFragmentOffset,
+            timeToLive,
+            `protocol`.rawValue,
+            headerChecksum,
+            sourceIpAddress.rawValue,
+            destinationIpAddress.rawValue
+        )
+        #endif
+    }
+}
+
+private func onesComplementAdd<Integer: FixedWidthInteger>(lhs: Integer, rhs: Integer) -> Integer {
+    var (sum, overflowed) = lhs.addingReportingOverflow(rhs)
+    if overflowed {
+        sum &+= 1
+    }
+    return sum
+}
+
+extension IPv4Header: CustomStringConvertible {
+    var description: String {
+        """
+        Version: \(version)
+        Header Length: \(internetHeaderLength * 4) bytes
+        Differentiated Services: \(String(differentiatedServicesCodePoint, radix: 2))
+        Explicit Congestion Notification: \(String(explicitCongestionNotification, radix: 2))
+        Total Length: \(totalLength) bytes
+        Identification: \(identification)
+        Flags: \(String(flags, radix: 2))
+        Fragment Offset: \(fragmentOffset) Bytes
+        Time to Live: \(timeToLive)
+        Protocol: \(`protocol`)
+        Header Checksum: \(headerChecksum) (\(isValidChecksum() ? "valid" : "*not* valid"))
+        Source IP Address: \(sourceIpAddress)
+        Destination IP Address: \(destinationIpAddress)
+        """
+    }
+}
--- Tests/NIOPosixTests/RawSocketBootstrapTests.swift
+++ Tests/NIOPosixTests/RawSocketBootstrapTests.swift
@@ -30,85 +30,6 @@ func XCTSkipIfUserHasNotEnoughRightsForRawSocketAPI(file: StaticString = #filePa
     try XCTSkipIf(geteuid() != 0, "Raw Socket API requires root privileges", file: file, line: line)
 }
 
-struct IPv4Address: Hashable {
-    var rawValue: UInt32
-}
-
-extension IPv4Address {
-    init(_ v1: UInt8, _ v2: UInt8, _ v3: UInt8, _ v4: UInt8) {
-        rawValue = UInt32(v1) << 24 | UInt32(v2) << 16 | UInt32(v3) << 8 | UInt32(v4)
-    }
-}
-
-struct IPv4Header {
-    static let size: Int = 20
-    
-    private let versionAndIhl: UInt8
-    var version: UInt8 {
-        versionAndIhl >> 4
-    }
-    var internetHeaderLength: UInt8 {
-        versionAndIhl & 0b0000_1111
-    }
-    private let dscpAndEcn: UInt8
-    var differentiatedServicesCodePoint: UInt8 {
-        dscpAndEcn >> 2
-    }
-    var explicitCongestionNotification: UInt8 {
-        dscpAndEcn & 0b0000_0011
-    }
-    let totalLength: UInt16
-    let identification: UInt16
-    let flagsAndFragmentOffset: UInt16
-    var flags: UInt8 {
-        UInt8(flagsAndFragmentOffset >> 13)
-    }
-    var fragmentOffset: UInt16 {
-        flagsAndFragmentOffset & 0b0001_1111_1111_1111
-    }
-    let timeToLive: UInt8
-    let `protocol`: NIOIPProtocol
-    let headerChecksum: UInt16
-    let sourceIpAdress: IPv4Address
-    let destinationIpAddress: IPv4Address
-    
-    init?(buffer: inout ByteBuffer) {
-        guard let (
-            versionAndIhl,
-            dscpAndEcn,
-            totalLength,
-            identification,
-            flagsAndFragmentOffset,
-            timeToLive,
-            `protocol`,
-            headerChecksum,
-            sourceIpAdress,
-            destinationIpAddress
-        ) = buffer.readMultipleIntegers(as: (
-            UInt8,
-            UInt8,
-            UInt16,
-            UInt16,
-            UInt16,
-            UInt8,
-            UInt8,
-            UInt16,
-            UInt32,
-            UInt32
-        ).self) else { return nil }
-        self.versionAndIhl = versionAndIhl
-        self.dscpAndEcn = dscpAndEcn
-        self.totalLength = totalLength
-        self.identification = identification
-        self.flagsAndFragmentOffset = flagsAndFragmentOffset
-        self.timeToLive = timeToLive
-        self.`protocol` = .init(rawValue: `protocol`)
-        self.headerChecksum = headerChecksum
-        self.sourceIpAdress = .init(rawValue: sourceIpAdress)
-        self.destinationIpAddress = .init(rawValue: destinationIpAddress)
-    }
-}
-
 final class RawSocketBootstrapTests: XCTestCase {
     func testBindWithRecevMmsg() throws {
         try XCTSkipIfUserHasNotEnoughRightsForRawSocketAPI()
@@ -122,7 +43,7 @@ final class RawSocketBootstrapTests: XCTestCase {
             .bind(host: "127.0.0.1", ipProtocol: .reservedForTesting).wait()
         defer { XCTAssertNoThrow(try channel.close().wait()) }
         try channel.configureForRecvMmsg(messageCount: 10)
-        let expectedMessages = (0..<10).map { "Hello World \($0)" }
+        let expectedMessages = (1...10).map { "Hello World \($0)" }
         for message in expectedMessages {
             _ = try channel.write(AddressedEnvelope(
                 remoteAddress: SocketAddress(ipAddress: "127.0.0.1", port: 0),
@@ -136,8 +57,19 @@ final class RawSocketBootstrapTests: XCTestCase {
             let header = try XCTUnwrap(IPv4Header(buffer: &data))
             XCTAssertEqual(header.version, 4)
             XCTAssertEqual(header.protocol, .reservedForTesting)
+            #if canImport(Darwin)
+            // On BSD the IP header will only contain the size of the ip packet body not the header.
+            // This is known bug which can't be fixed without breaking old apps which already workaround the issue
+            // like we do.
+            XCTAssertEqual(Int(header.totalLength), data.readableBytes)
+            // On BSD the checksum is always zero
+            XCTAssertEqual(header.headerChecksum, 0)
+            #elseif os(Linux)
             XCTAssertEqual(Int(header.totalLength), IPv4Header.size + data.readableBytes)
-            XCTAssertEqual(header.sourceIpAdress, .init(127, 0, 0, 1))
+            XCTAssertTrue(header.isValidChecksum(), "\(header)")
+            #endif
+            
+            XCTAssertEqual(header.sourceIpAddress, .init(127, 0, 0, 1))
             XCTAssertEqual(header.destinationIpAddress, .init(127, 0, 0, 1))
             return String(buffer: data)
         })
@@ -164,7 +96,7 @@ final class RawSocketBootstrapTests: XCTestCase {
             .bind(host: "127.0.0.1", ipProtocol: .reservedForTesting).wait()
         defer { XCTAssertNoThrow(try writeChannel.close().wait()) }
         
-        let expectedMessages = (0..<10).map { "Hello World \($0)" }
+        let expectedMessages = (1...10).map { "Hello World \($0)" }
         for message in expectedMessages {
             _ = try writeChannel.write(AddressedEnvelope(
                 remoteAddress: SocketAddress(ipAddress: "127.0.0.1", port: 0),
@@ -178,8 +110,19 @@ final class RawSocketBootstrapTests: XCTestCase {
             let header = try XCTUnwrap(IPv4Header(buffer: &data))
             XCTAssertEqual(header.version, 4)
             XCTAssertEqual(header.protocol, .reservedForTesting)
+            #if canImport(Darwin)
+            // On BSD the IP header will only contain the size of the ip packet body not the header.
+            // This is known bug which can't be fixed without breaking old apps which already workaround the issue
+            // like we do.
+            XCTAssertEqual(Int(header.totalLength), data.readableBytes)
+            // On BSD the checksum is always zero
+            XCTAssertEqual(header.headerChecksum, 0)
+            #elseif os(Linux)
             XCTAssertEqual(Int(header.totalLength), IPv4Header.size + data.readableBytes)
-            XCTAssertEqual(header.sourceIpAdress, .init(127, 0, 0, 1))
+            XCTAssertTrue(header.isValidChecksum(), "\(header)")
+            #endif
+            XCTAssertTrue(header.isValidChecksum())
+            XCTAssertEqual(header.sourceIpAddress, .init(127, 0, 0, 1))
             XCTAssertEqual(header.destinationIpAddress, .init(127, 0, 0, 1))
             return String(buffer: data)
         })
@@ -187,4 +130,59 @@ final class RawSocketBootstrapTests: XCTestCase {
         XCTAssertEqual(receivedMessages, Set(expectedMessages))
     }
     
+    func testIpHdrincl() throws {
+        try XCTSkipIfUserHasNotEnoughRightsForRawSocketAPI()
+        
+        let elg = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+        defer { XCTAssertNoThrow(try elg.syncShutdownGracefully()) }
+        let channel = try RawSocketBootstrap(group: elg)
+            .channelOption(ChannelOptions.ipOption(.ip_hdrincl), value: 1)
+            .channelInitializer {
+                $0.pipeline.addHandler(DatagramReadRecorder<ByteBuffer>(), name: "ByteReadRecorder")
+            }
+            .bind(host: "127.0.0.1", ipProtocol: .reservedForTesting).wait()
+        defer { XCTAssertNoThrow(try channel.close().wait()) }
+        try channel.configureForRecvMmsg(messageCount: 10)
+        let expectedMessages = (1...10).map { "Hello World \($0)" }
+        for message in expectedMessages.map(ByteBuffer.init(string:)) {
+            var packet = ByteBuffer()
+            var header = IPv4Header()
+            header.version = 4
+            header.internetHeaderLength = 5
+            header.totalLength = UInt16(IPv4Header.size + message.readableBytes)
+            header.protocol = .reservedForTesting
+            header.timeToLive = 64
+            header.destinationIpAddress = .init(127, 0, 0, 1)
+            header.sourceIpAddress = .init(127, 0, 0, 1)
+            header.setChecksum()
+            header.write(to: &packet)
+            packet.writeImmutableBuffer(message)
+            _ = try channel.write(AddressedEnvelope(
+                remoteAddress: SocketAddress(ipAddress: "127.0.0.1", port: 0),
+                data: packet
+            ))
+        }
+        channel.flush()
+        
+        let receivedMessages = Set(try channel.waitForDatagrams(count: 10).map { envelop -> String in
+            var data = envelop.data
+            let header = try XCTUnwrap(IPv4Header(buffer: &data))
+            XCTAssertEqual(header.version, 4)
+            XCTAssertEqual(header.protocol, .reservedForTesting)
+            #if canImport(Darwin)
+            // On BSD the IP header will only contain the size of the ip packet body not the header.
+            // This is known bug which can't be fixed without breaking old apps which already workaround the issue
+            // like we do.
+            XCTAssertEqual(Int(header.totalLength), data.readableBytes)
+            #elseif os(Linux)
+            XCTAssertEqual(Int(header.totalLength), IPv4Header.size + data.readableBytes)
+            #endif
+            XCTAssertTrue(header.isValidChecksum())
+            XCTAssertEqual(header.sourceIpAddress, .init(127, 0, 0, 1))
+            XCTAssertEqual(header.destinationIpAddress, .init(127, 0, 0, 1))
+            return String(buffer: data)
+        })
+        
+        XCTAssertEqual(receivedMessages, Set(expectedMessages))
+    }
 }
