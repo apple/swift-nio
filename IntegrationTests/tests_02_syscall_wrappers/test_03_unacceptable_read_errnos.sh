1ce136b4c392bd7427cf408d65305a05d162fb46
David Nadoba <d_nadoba@apple.com>
2023-01-09 19:34:48 +0100

    Fix main nightly CI (#2337)
    * Fix nightly CI
    
    * conditionalization of signal for architecture

--- IntegrationTests/tests_02_syscall_wrappers/test_03_unacceptable_read_errnos.sh
+++ IntegrationTests/tests_02_syscall_wrappers/test_03_unacceptable_read_errnos.sh
@@ -87,8 +87,17 @@ for mode in debug release; do
             fail "exited successfully but was supposed to fail"
         else
             exit_code=$?
-            # expecting illegal instruction as it should fail with an unacceptable errno
-            assert_equal $(( 128 + 4 )) $exit_code  # 4 == SIGILL
+            
+            # expecting irrecoverable error as process should be terminated through fatalError/precondition/assert
+            architecture=$(uname -m)
+            if [[ $architecture =~ ^(arm|aarch) ]]; then
+                assert_equal $exit_code $(( 128 + 5 )) # 5 == SIGTRAP aka trace trap, expected on ARM
+            elif [[ $architecture =~ ^(x86|i386) ]]; then
+                assert_equal $exit_code $(( 128 + 4 ))  # 4 == SIGILL aka illegal instruction, expected on x86
+            else
+                fail "unknown CPU architecture for which we don't know the expected signal for a crash"
+            fi
+            
             if [[ "$mode" == "debug" ]]; then
                 grep -q unacceptable\ errno "$temp_file"
             fi
