73cf8b261be92d7fc46aa6366fb9d857a082df41
Joakim Hassila <jocke@ordo.one>
2023-03-16 09:00:47 +0100

    Change license headers to fix soundness check

--- Benchmarks/Benchmarks/HTTPRequests/HTTPRequests.swift
+++ Benchmarks/Benchmarks/HTTPRequests/HTTPRequests.swift
@@ -1,12 +1,16 @@
+//===----------------------------------------------------------------------===//
 //
-// Copyright (c) 2022 Ordo One AB.
+// This source file is part of the SwiftNIO open source project
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Copyright (c) 2020-2023 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
 //
-// You may obtain a copy of the License at
-// http://www.apache.org/licenses/LICENSE-2.0
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
 //
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 import NIOPerformanceTester
 import NIOEmbedded
