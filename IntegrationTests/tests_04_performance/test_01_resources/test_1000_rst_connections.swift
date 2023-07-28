89d643e80620c20b9e8237d881926e22c090e740
Franz Busch <f.busch@apple.com>
2023-07-28 11:51:03 +0100

    Fix 5.9 and nightly allocation limits
    # Motivation
    Our 5.9 and nightly CI has been failing for some time now due to flaky allocation tests.
    
    # Modification
    This PR slightly changes some shared infra for how we ran some of our allocation tests. We are now waiting for the client and server channel to close so that allocations are more stable.
    
    # Results
    Green CI again

--- IntegrationTests/tests_04_performance/test_01_resources/test_1000_rst_connections.swift
+++ IntegrationTests/tests_04_performance/test_01_resources/test_1000_rst_connections.swift
@@ -15,6 +15,17 @@
 import NIOCore
 import NIOPosix
 
+private final class CloseAfterTimeoutHandler: ChannelInboundHandler {
+    typealias InboundIn = ByteBuffer
+
+    func channelActive(context: ChannelHandlerContext) {
+        context.fireChannelActive()
+        context.eventLoop.scheduleTask(in: .milliseconds(1)) {
+            context.close(promise: nil)
+        }
+    }
+}
+
 func run(identifier: String) {
     let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
     defer {
@@ -27,6 +38,11 @@ func run(identifier: String) {
     
     let serverAddress = serverConnection.localAddress!
     let clientBootstrap = ClientBootstrap(group: group)
+        .channelInitializer { channel in
+            channel.eventLoop.makeCompletedFuture {
+                try channel.pipeline.syncOperations.addHandler(CloseAfterTimeoutHandler())
+            }
+        }
 
     measure(identifier: identifier, trackFDs: true) {
         let iterations = 1000
@@ -35,7 +51,7 @@ func run(identifier: String) {
             
             let _: Void? = try? conn.flatMap { channel in
                 (channel as! SocketOptionProvider).setSoLinger(linger(l_onoff: 1, l_linger: 0)).flatMap {
-                    channel.close()
+                    channel.closeFuture
                 }
             }.wait()
         }
