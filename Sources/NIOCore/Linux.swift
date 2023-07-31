e20271f4a3c055a9035038b9f53b9d4e71370ea3
Peter Adams <63288215+PeterAdams-A@users.noreply.github.com>
2023-07-31 13:50:24 +0100

    Make System.coreCount aware of cgroup2 (#2394)
    Motivation:
    
    The count already appears to be aware of cgroup1
    but doesn't have logic to support cgroup2.
    
    Modifications:
    
    Add logic to parse /sys/fs/cgroup/cpu.max
    For details see - https://stackoverflow.com/questions/65551215/get-docker-cpu-memory-limit-inside-container/65554131#65554131
    
    Result:
    
    Core count will adapt if constrained by a cgroup on more modern kernels.
    
    ---------
    
    Co-authored-by: Max Desiatov <m_desiatov@apple.com>
    Co-authored-by: Gwynne Raskind <gwynne@darkrainfall.org>

--- Sources/NIOCore/Linux.swift
+++ Sources/NIOCore/Linux.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the SwiftNIO open source project
 //
-// Copyright (c) 2017-2018 Apple Inc. and the SwiftNIO project authors
+// Copyright (c) 2017-2023 Apple Inc. and the SwiftNIO project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
@@ -21,6 +21,7 @@ enum Linux {
     static let cfsQuotaPath = "/sys/fs/cgroup/cpu/cpu.cfs_quota_us"
     static let cfsPeriodPath = "/sys/fs/cgroup/cpu/cpu.cfs_period_us"
     static let cpuSetPath = "/sys/fs/cgroup/cpuset/cpuset.cpus"
+    static let cfsCpuMaxPath = "/sys/fs/cgroup/cpu.max"
 
     private static func firstLineOfFile(path: String) throws -> Substring {
         let fh = try NIOFileHandle(path: path)
@@ -59,7 +60,10 @@ enum Linux {
         return cpuset.map(countCoreIds).reduce(0, +)
     }
 
-    static func coreCount(quota quotaPath: String,  period periodPath: String) -> Int? {
+    /// Get the available core count according to cgroup1 restrictions.
+    /// Round up to the next whole number.
+    static func coreCountCgroup1Restriction(quota quotaPath: String = Linux.cfsQuotaPath,
+                                            period periodPath: String = Linux.cfsPeriodPath) -> Int? {
         guard
             let quota = try? Int(firstLineOfFile(path: quotaPath)),
             quota > 0
@@ -67,6 +71,17 @@ enum Linux {
         guard
             let period = try? Int(firstLineOfFile(path: periodPath)),
             period > 0
+        else { return nil }
+        return (quota - 1 + period) / period // always round up if fractional CPU quota requested
+    }
+
+    /// Get the available core count according to cgroup2 restrictions.
+    /// Round up to the next whole number.
+    static func coreCountCgroup2Restriction(cpuMaxPath: String = Linux.cfsCpuMaxPath) -> Int? {
+        guard let maxDetails = try? firstLineOfFile(path: cpuMaxPath),
+              let spaceIndex = maxDetails.firstIndex(of: " "),
+              let quota = Int(maxDetails[maxDetails.startIndex ..< spaceIndex]),
+              let period = Int(maxDetails[maxDetails.index(after: spaceIndex) ..< maxDetails.endIndex])
         else { return nil }
         return (quota - 1 + period) / period // always round up if fractional CPU quota requested
     }
