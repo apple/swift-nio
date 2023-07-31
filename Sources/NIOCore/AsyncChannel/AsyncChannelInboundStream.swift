fd3076131f3bddf5bce0002358914ff66fa431c3
Franz Busch <f.busch@apple.com>
2023-07-31 11:29:41 +0100

    Add testing interfaces for the stream and writer of the `NIOAsyncChannel` (#2488)
    # Motivation
    When using the `NIOAsyncChannel` to build higher level abstractions it is hard to test right now since one cannot drive the `NIOAsyncChannelInputStream` nor the `NIOAsyncChannelOutputWriter`.
    
    # Modification
    This PR adds new factory methods to both of those types which allow to either drive the input stream or record the outputs written to the output writer.
    
    # Result
    One can now successfully test their code written on top of the input stream and output writer.

--- Sources/NIOCore/AsyncChannel/AsyncChannelInboundStream.swift
+++ Sources/NIOCore/AsyncChannel/AsyncChannelInboundStream.swift
@@ -21,8 +21,68 @@ public struct NIOAsyncChannelInboundStream<Inbound: Sendable>: Sendable {
     @usableFromInline
     typealias Producer = NIOThrowingAsyncSequenceProducer<Inbound, Error, NIOAsyncSequenceProducerBackPressureStrategies.HighLowWatermark, NIOAsyncChannelInboundStreamChannelHandlerProducerDelegate>
 
+    /// A source used for driving a ``NIOAsyncChannelInboundStream`` during tests.
+    public struct TestSource {
+        @usableFromInline
+        internal let continuation: AsyncStream<Inbound>.Continuation
+
+        @inlinable
+        init(continuation: AsyncStream<Inbound>.Continuation) {
+            self.continuation = continuation
+        }
+
+        /// Yields the element to the inbound stream.
+        ///
+        /// - Parameter element: The element to yield to the inbound stream.
+        @inlinable
+        public func yield(_ element: Inbound) {
+            self.continuation.yield(element)
+        }
+
+        /// Finished the inbound stream.
+        @inlinable
+        public func finish() {
+            self.continuation.finish()
+        }
+    }
+
+    #if swift(>=5.7)
+    @usableFromInline
+    enum _Backing: Sendable {
+        case asyncStream(AsyncStream<Inbound>)
+        case producer(Producer)
+    }
+    #else
+    // AsyncStream wasn't marked as `Sendable` in 5.6
+    @usableFromInline
+    enum _Backing: @unchecked Sendable {
+        case asyncStream(AsyncStream<Inbound>)
+        case producer(Producer)
+    }
+    #endif
+
     /// The underlying async sequence.
-    @usableFromInline let _producer: Producer
+    @usableFromInline
+    let _backing: _Backing
+
+    /// Creates a new stream with a source for testing.
+    ///
+    /// This is useful for writing unit tests where you want to drive a ``NIOAsyncChannelInboundStream``.
+    ///
+    /// - Returns: A tuple containing the input stream and a test source to drive it.
+    @inlinable
+    public static func makeTestingStream() -> (Self, TestSource) {
+        var continuation: AsyncStream<Inbound>.Continuation!
+        let stream = AsyncStream<Inbound> { continuation = $0 }
+        let source = TestSource(continuation: continuation)
+        let inputStream = Self(stream: stream)
+        return (inputStream, source)
+    }
+
+    @inlinable
+    init(stream: AsyncStream<Inbound>) {
+        self._backing = .asyncStream(stream)
+    }
 
     @inlinable
     init<HandlerInbound: Sendable>(
@@ -48,7 +108,7 @@ public struct NIOAsyncChannelInboundStream<Inbound: Sendable>: Sendable {
         )
         handler.source = sequence.source
         try channel.pipeline.syncOperations.addHandler(handler)
-        self._producer = sequence.sequence
+        self._backing = .producer(sequence.sequence)
     }
 
     /// Creates a new ``NIOAsyncChannelInboundStream`` which is used when the pipeline got synchronously wrapped.
@@ -101,23 +161,42 @@ extension NIOAsyncChannelInboundStream: AsyncSequence {
 
     @_spi(AsyncChannel)
     public struct AsyncIterator: AsyncIteratorProtocol {
-        @usableFromInline var _iterator: Producer.AsyncIterator
+        @usableFromInline
+        enum _Backing {
+            case asyncStream(AsyncStream<Inbound>.Iterator)
+            case producer(Producer.AsyncIterator)
+        }
+
+        @usableFromInline var _backing: _Backing
 
         @inlinable
-        init(_ iterator: Producer.AsyncIterator) {
-            self._iterator = iterator
+        init(_ backing: NIOAsyncChannelInboundStream<Inbound>._Backing) {
+            switch backing {
+            case .asyncStream(let asyncStream):
+                self._backing = .asyncStream(asyncStream.makeAsyncIterator())
+            case .producer(let producer):
+                self._backing = .producer(producer.makeAsyncIterator())
+            }
         }
 
         @inlinable @_spi(AsyncChannel)
         public mutating func next() async throws -> Element? {
-            return try await self._iterator.next()
+            switch self._backing {
+            case .asyncStream(var iterator):
+                let value = await iterator.next()
+                self._backing = .asyncStream(iterator)
+                return value
+
+            case .producer(let iterator):
+                return try await iterator.next()
+            }
         }
     }
 
     @inlinable
     @_spi(AsyncChannel)
     public func makeAsyncIterator() -> AsyncIterator {
-        return AsyncIterator(self._producer.makeAsyncIterator())
+        return AsyncIterator(self._backing)
     }
 }
 
