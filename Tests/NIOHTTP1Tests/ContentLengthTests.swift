18962c4e2996cf38644ea842a577b46477c8b1c4
hamzahrmalik <hamzahrmalik@yahoo.co.uk>
2023-11-28 17:49:51 +0000

    Apply suggestions from code review
    Co-authored-by: Franz Busch <privat@franz-busch.de>

--- Tests/NIOHTTP1Tests/ContentLengthTests.swift
+++ Tests/NIOHTTP1Tests/ContentLengthTests.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the SwiftNIO open source project
 //
-// Copyright (c) 2017-2021 Apple Inc. and the SwiftNIO project authors
+// Copyright (c) 2023 Apple Inc. and the SwiftNIO project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
@@ -17,7 +17,7 @@ import NIOCore
 import NIOEmbedded
 import NIOHTTP1
 
-class ContentLengthTests: XCTestCase {
+final class ContentLengthTests: XCTestCase {
 
     /// Client receives a response longer than the content-length header
     func testResponseContentTooLong() throws {
