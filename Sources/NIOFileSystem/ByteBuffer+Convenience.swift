0e993e0c6aa8301694444582f0d23e291778710a
George Barnett <gbarnett@apple.com>
2024-01-08 09:00:06 +0000

    Add ByteBuffer convenience for writing

--- Sources/NIOFileSystem/ByteBuffer+Convenience.swift
+++ Sources/NIOFileSystem/ByteBuffer+Convenience.swift
@@ -50,4 +50,48 @@ extension ByteBuffer {
             fileSystem: .shared
         )
     }
+
+    /// Writes the readable bytes of the ``ByteBuffer`` to a file.
+    ///
+    /// - Parameters:
+    ///   - path: The path of the file to write the contents of the sequence to.
+    ///   - offset: The offset into the file to write to, defaults to zero.
+    ///   - options: Options for opening the file, defaults to creating a new file.
+    ///   - fileSystem: The ``FileSystemProtocol`` instance to use.
+    /// - Returns: The number of bytes written to the file.
+    @discardableResult
+    public func write(
+        toFileAt path: FilePath,
+        absoluteOffset offset: Int64 = 0,
+        options: OpenOptions.Write = .newFile(replaceExisting: false),
+        fileSystem: some FileSystemProtocol
+    ) async throws -> Int64 {
+        try await self.readableBytesView.write(
+            toFileAt: path,
+            absoluteOffset: offset,
+            options: options,
+            fileSystem: fileSystem
+        )
+    }
+
+    /// Writes the readable bytes of the ``ByteBuffer`` to a file.
+    ///
+    /// - Parameters:
+    ///   - path: The path of the file to write the contents of the sequence to.
+    ///   - offset: The offset into the file to write to, defaults to zero.
+    ///   - options: Options for opening the file, defaults to creating a new file.
+    /// - Returns: The number of bytes written to the file.
+    @discardableResult
+    public func write(
+        toFileAt path: FilePath,
+        absoluteOffset offset: Int64 = 0,
+        options: OpenOptions.Write = .newFile(replaceExisting: false)
+    ) async throws -> Int64 {
+        try await self.write(
+            toFileAt: path,
+            absoluteOffset: offset,
+            options: options,
+            fileSystem: .shared
+        )
+    }
 }
