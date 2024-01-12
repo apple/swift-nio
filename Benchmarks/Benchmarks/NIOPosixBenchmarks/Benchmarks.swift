826fce4fdbe458f3b8c30b1e2e7b4f24a37d2806
Franz Busch <f.busch@apple.com>
2024-01-12 14:23:54 +0000

    Fix `NIOAsyncChannel` allocation benchmarks (#2622)
    # Motivation
    We had to disable the benchmarks since they regressed without us noticing and they appear to be flaky.
    
    # Modification
    This PR fixes the allocation regression and tries to re-enable them.

--- Benchmarks/Benchmarks/NIOPosixBenchmarks/Benchmarks.swift
+++ Benchmarks/Benchmarks/NIOPosixBenchmarks/Benchmarks.swift
@@ -39,28 +39,29 @@ let benchmarks = {
     // This benchmark is only available above 5.9 since our EL conformance
     // to serial executor is also gated behind 5.9.
     #if compiler(>=5.9)
-// In addition this benchmark currently doesn't produce deterministic results on our CI
-// and therefore is currently disabled
-//    Benchmark(
-//        "TCPEchoAsyncChannel",
-//        configuration: .init(
-//            metrics: defaultMetrics,
-//            timeUnits: .milliseconds,
-//            scalingFactor: .mega,
-//            setup: {
-//                swiftTaskEnqueueGlobalHook = { job, _ in
-//                    eventLoop.executor.enqueue(job)
-//                }
-//            },
-//            teardown: {
-//                swiftTaskEnqueueGlobalHook = nil
-//            }
-//        )
-//    ) { benchmark in
-//        try await runTCPEchoAsyncChannel(
-//            numberOfWrites: benchmark.scaledIterations.upperBound,
-//            eventLoop: eventLoop
-//        )
-//    }
+    Benchmark(
+        "TCPEchoAsyncChannel",
+        configuration: .init(
+            metrics: defaultMetrics,
+            timeUnits: .milliseconds,
+            scalingFactor: .mega,
+            // We are expecting a bit of allocation variance due to an allocation
+            // in the Concurrency runtime which happens when resuming a continuation.
+            thresholds: [.mallocCountTotal: .init(absolute: [.p90: 2000])],
+            setup: {
+                swiftTaskEnqueueGlobalHook = { job, _ in
+                    eventLoop.executor.enqueue(job)
+                }
+            },
+            teardown: {
+                swiftTaskEnqueueGlobalHook = nil
+            }
+        )
+    ) { benchmark in
+        try await runTCPEchoAsyncChannel(
+            numberOfWrites: benchmark.scaledIterations.upperBound,
+            eventLoop: eventLoop
+        )
+    }
     #endif
 }
