a0e0c68d71bc9a31e35576992665949d1d75f69d
Harish Yerra <hyerra@gmail.com>
2024-01-17 05:34:01 -0800

    [arch] Add arm64_32 to the 32-bit platform check (#2625)
    On watchOS, the arm64_32 bit architecture restricts software to ILP32 which means integers are 32-bit. This expands our existing 32-bit checks to include arm64_32.

--- Tests/NIOCoreTests/ByteBufferTest.swift
+++ Tests/NIOCoreTests/ByteBufferTest.swift
@@ -1327,7 +1327,7 @@ class ByteBufferTest: XCTestCase {
     }
 
     func testAllocationOfReallyBigByteBuffer() throws {
-        #if arch(arm) || arch(i386)
+        #if arch(arm) || arch(i386) || arch(arm64_32)
         // this test doesn't work on 32-bit platforms because the address space is only 4GB large and we're trying
         // to make a 4GB ByteBuffer which just won't fit. Even going down to 2GB won't make it better.
         return
