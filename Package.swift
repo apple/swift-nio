ff4c69ee2ffe26ba1f1967e9b6cc126b2ba25934
George Barnett <gbarnett@apple.com>
2024-03-08 14:23:38 +0000

    Raise minimum Swift version to 5.8

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.8
 //===----------------------------------------------------------------------===//
 //
 // This source file is part of the SwiftNIO open source project
