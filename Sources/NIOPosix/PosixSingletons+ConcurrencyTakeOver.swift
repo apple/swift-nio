5c378ed31ceaf21bf9ceb93433054b28ac1184da
Johannes Weiss <johannesweiss@apple.com>
2024-05-14 11:12:13 +0100

    Document unsafeTryInstallSingletonPosixEventLoopGroupAsConcurrencyGlobalExecutor (#2721)
    Document what unsafeTryInstallSingletonPosixEventLoopGroupAsConcurrencyGlobalExecutor's return value means

--- Sources/NIOPosix/PosixSingletons+ConcurrencyTakeOver.swift
+++ Sources/NIOPosix/PosixSingletons+ConcurrencyTakeOver.swift
@@ -35,6 +35,8 @@ extension NIOSingletons {
     /// operation doesn't guarantee anything because another piece of code could have done the same without using atomic operations. But we
     /// do our best.
     ///
+    /// - returns: If ``MultiThreadedEventLoopGroup/singleton`` was successfully installed as Swift Concurrency's global executor or not.
+    ///
     /// - warning: You may only call this method from the main thread.
     /// - warning: You may only call this method once.
     @discardableResult
