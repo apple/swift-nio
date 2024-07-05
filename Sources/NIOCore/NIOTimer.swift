b0d3f66cd8f337a4531b1e287958e76a216b76f1
Si Beaumont <beaumont@apple.com>
2024-07-05 09:10:27 +0100

    feedback: Make NIOTimer Sendable

--- Sources/NIOCore/NIOTimer.swift
+++ Sources/NIOCore/NIOTimer.swift
@@ -24,9 +24,9 @@ public protocol NIOTimerHandler {
 /// Users cannot create an instance of this type; it is returned by ``EventLoop/setTimer(for:_:)-5e37g``.
 ///
 /// - Seealso: ``EventLoop/setTimer(for:_:)-5e37g``.
-public struct NIOTimer {
+public struct NIOTimer: Sendable {
     @usableFromInline
-    enum Backing {
+    enum Backing: Sendable {
         /// A task created using `EventLoop.scheduleTask(deadline:_:)`, used by default for `EventLoop` implementations.
         case scheduledTask(Scheduled<Void>)
         /// An identifier for a timer, used by `EventLoop` implementations that conform to `CustomTimerImplementation`.
@@ -79,9 +79,13 @@ public protocol NIOCustomTimerImplementation: EventLoop {
     /// Set a timer that calls handler at the given time.
     ///
     /// Implementations must return an integer identifier that uniquely identifies the timer.
+    ///
+    /// Implementations should tolerate this call being made off the event loop.
     func setTimer(for deadline: NIODeadline, _ handler: any NIOTimerHandler) -> UInt64
 
     /// Cancel a timer with a given timer identifier.
+    ///
+    /// Implementations should tolerate this call being made off the event loop.
     func cancelTimer(_ id: UInt64)
 }
 
