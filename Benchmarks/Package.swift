654bf41c03e99791f2e2c654e3974cf80aadeb0c
Franz Busch <f.busch@apple.com>
2024-07-12 12:07:06 +0200

    [GHA] Add license header check (#2781)
    * [GHA] Add license header check
    
    # Motivation
    
    We need to make sure all code files have the appropriate license headers in place. This is currently part of the soundness check and we need to replace this with a GH action.
    
    # Modification
    
    Adds a new job to the reusable workflow the check all files for license headers. Since some files are ignored every repo can specify a `.licenseignore` file.
    
    # Result
    
    Last part of the soundness script migrated.
    
    * Review
    
    * Remove default excludes

--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -1,17 +1,4 @@
 // swift-tools-version: 5.7
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the SwiftCertificates open source project
-//
-// Copyright (c) 2023 Apple Inc. and the SwiftCertificates project authors
-// Licensed under Apache License v2.0
-//
-// See LICENSE.txt for license information
-// See CONTRIBUTORS.txt for the list of SwiftCertificates project authors
-//
-// SPDX-License-Identifier: Apache-2.0
-//
-//===----------------------------------------------------------------------===//
 
 import PackageDescription
 
