d7739e8b62c21d5f2a7d712fe5cd87fec892eb29
Franz Busch <f.busch@apple.com>
2024-07-12 14:39:28 +0200

    [GHA] Cxx interoperability compatibility and integration tests check
    # Motivation
    
    Another reusable check is to make sure that all library products of a package are successfully building when consumed from a module that has Cxx interoperability enabled. Another check that's missing is running the integration tests.
    
    # Modification
    
    This PR adds two new checks to the reusable workflow. One to check for Cxx interoperability compatibility and another one to run the integration tests. I also fixed a misalgined name for the nightly benchmarks.
    
    # Result
    
    This should be one of the last reusable workflow checks.

--- IntegrationTests/tests_04_performance/test_01_allocation_counts.sh
+++ IntegrationTests/tests_04_performance/test_01_allocation_counts.sh
@@ -18,6 +18,8 @@ source defines.sh
 set -eu
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+test -n "${SWIFT_VERSION:-}" || fatal "SWIFT_VERSION unset"
+
 all_tests=()
 for file in "$here/test_01_resources/"test_*.swift; do
     test_name=$(basename "$file")
@@ -37,6 +39,7 @@ for test in "${all_tests[@]}"; do
         not_freed_allocations=$(grep "^test_$test_case.remaining_allocations:" "$tmp/output" | cut -d: -f2 | sed 's/ //g')
         leaked_fds=$(grep "^test_$test_case.leaked_fds:" "$tmp/output" | cut -d: -f2 | sed 's/ //g')
         max_allowed_env_name="MAX_ALLOCS_ALLOWED_$test_case"
+        max_allowed=$(jq '.'\"$test_case\" "$here/Thresholds/$SWIFT_VERSION.json")
 
         info "$test_case: allocations not freed: $not_freed_allocations"
         info "$test_case: total number of mallocs: $total_allocations"
@@ -45,14 +48,17 @@ for test in "${all_tests[@]}"; do
         assert_less_than "$not_freed_allocations" 5     # allow some slack
         assert_greater_than "$not_freed_allocations" -5 # allow some slack
         assert_less_than "$leaked_fds" 1  # No slack allowed here though
-        if [[ -z "${!max_allowed_env_name+x}" ]]; then
+        if [[ -z "${!max_allowed_env_name+x}" ]] && [ -z "${max_allowed}" ]; then
             if [[ -z "${!max_allowed_env_name+x}" ]]; then
                 warn "no reference number of allocations set (set to \$$max_allowed_env_name)"
-                warn "to set current number:"
+                warn "to set current number either:"
                 warn "    export $max_allowed_env_name=$total_allocations"
+                warn "    or set them in the Swift version specific threshold json"
             fi
         else
-            max_allowed=${!max_allowed_env_name}
+            if [ -z "${max_allowed}" ]; then
+                max_allowed=${!max_allowed_env_name}
+            fi
             assert_less_than_or_equal "$total_allocations" "$max_allowed"
             assert_greater_than "$total_allocations" "$(( max_allowed - 1000))"
         fi
