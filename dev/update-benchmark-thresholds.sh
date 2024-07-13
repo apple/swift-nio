7b03cb247bde066a2b87df517acfaf99436151e6
Franz Busch <f.busch@apple.com>
2024-07-13 11:51:05 +0200

    Fix benchmark thresholds update script (#2783)
    # Motivation
    
    We added new CI to run both nightly main and nightly next but forgot to update the benchmark thresholds script
    
    # Modification
    
    This PR updates the benchmark thresholds script and adds a nightly next docker compose image.
    
    # Result
    
    We can use the update benchmark thresholds script again.

--- dev/update-benchmark-thresholds.sh
+++ dev/update-benchmark-thresholds.sh
@@ -32,10 +32,10 @@ set -o pipefail
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 target_repo=${2-"$here/.."}
 
-for f in 57 58 59 510 -nightly; do
+for f in 58 59 510 main next; do
     echo "swift$f"
 
-    docker_file=$(if [[ "$f" == "-nightly" ]]; then f=main; fi && ls "$target_repo/docker/docker-compose."*"$f"*".yaml")
+    docker_file=$(ls "$target_repo/docker/docker-compose."*"$f"*".yaml")
 
     docker-compose -f docker/docker-compose.yaml -f $docker_file run update-benchmark-baseline
 done
