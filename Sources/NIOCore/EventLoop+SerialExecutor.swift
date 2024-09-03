8d95f6cf878dcd4e5e352db354efd6175715ba47
Fabian Fett <fabianfett@apple.com>
2024-09-03 16:08:47 +0200

    Fix other comment

--- Sources/NIOCore/EventLoop+SerialExecutor.swift
+++ Sources/NIOCore/EventLoop+SerialExecutor.swift
@@ -50,7 +50,7 @@ extension NIOSerialEventLoopExecutor {
         other === self
     }
 
-    @available(macOS 15.0, iOS 18.0, watchOS 11.0, tvOS 18.0, visionOS 2.0, *)
+    @available(macOS 15.0, iOS 18.0, watchOS 11.0, tvOS 18.0, *)
     @inlinable
     public func checkIsolated() {
         self.preconditionInEventLoop()
