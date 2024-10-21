cc1c57c2912a058870f4f47d84a001aeb7be39ac
Clinton Nkwocha <32041805+clintonpi@users.noreply.github.com>
2024-10-21 09:41:10 +0100

    Provide APIs to read file into more data types (#2923)
    Motivation:
    
    As requested in issues
    [#2875](https://github.com/apple/swift-nio/issues/2875) and
    [#2876](https://github.com/apple/swift-nio/issues/2876), it would be
    convenient to be able to read the contents of a file into more data
    types such as `Array`, `ArraySlice` & Foundation's `Data`.
    
    Modifications:
    
    - Extend `Array`, `ArraySlice` & `Data` to be initialisable with the
    contents of a file.
    
    Result:
    
    The contents of a file can be read into more data types.
    
    ---------
    
    Co-authored-by: George Barnett <gbarnett@apple.com>

--- Tests/NIOFileSystemFoundationCompatTests/FileSystemFoundationCompatTests.swift
+++ Tests/NIOFileSystemFoundationCompatTests/FileSystemFoundationCompatTests.swift
@@ -18,6 +18,37 @@ import _NIOFileSystemFoundationCompat
 import XCTest
 
 @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+extension FileSystem {
+    func temporaryFilePath(
+        _ function: String = #function,
+        inTemporaryDirectory: Bool = true
+    ) async throws -> FilePath {
+        if inTemporaryDirectory {
+            let directory = try await self.temporaryDirectory
+            return self.temporaryFilePath(function, inDirectory: directory)
+        } else {
+            return self.temporaryFilePath(function, inDirectory: nil)
+        }
+    }
+
+    func temporaryFilePath(
+        _ function: String = #function,
+        inDirectory directory: FilePath?
+    ) -> FilePath {
+        let index = function.firstIndex(of: "(")!
+        let functionName = function.prefix(upTo: index)
+        let random = UInt32.random(in: .min ... .max)
+        let fileName = "\(functionName)-\(random)"
+
+        if let directory = directory {
+            return directory.appending(fileName)
+        } else {
+            return FilePath(fileName)
+        }
+    }
+}
+
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
 final class FileSystemBytesConformanceTests: XCTestCase {
     func testTimepecToDate() async throws {
         XCTAssertEqual(
@@ -32,6 +63,19 @@ final class FileSystemBytesConformanceTests: XCTestCase {
             FileInfo.Timespec(seconds: 1, nanoseconds: 1).date,
             Date(timeIntervalSince1970: 1.000000001)
         )
+    }
+
+    func testReadFileIntoData() async throws {
+        let fs = FileSystem.shared
+        let path = try await fs.temporaryFilePath()
+
+        try await fs.withFileHandle(forReadingAndWritingAt: path) { fileHandle in
+            _ = try await fileHandle.write(contentsOf: [0, 1, 2], toAbsoluteOffset: 0)
+        }
+
+        let contents = try await Data(contentsOf: path, maximumSizeAllowed: .bytes(1024))
+
+        XCTAssertEqual(contents, Data([0, 1, 2]))
     }
 }
 #endif
