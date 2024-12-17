a227d912c7c9ddb397f3b0c1c1edf088ea764271
Cory Benfield <lukasa@apple.com>
2024-12-17 12:14:05 +0000

    Add Sendable annotations to the syscall wrappers
    Motivation
    
    Unfortunately there are a few places where we have to help
    the compiler out to realise that our copies of the libc functions
    are also Sendable, and -require-explicit-sendable can cause
    us some trouble if we don't annotate our usableFromInline types.
    
    Modifications
    
    Annotate our usableFromInline types
    Add some @Sendable
    
    Results
    
    The march continues

--- Sources/NIOPosix/System.swift
+++ Sources/NIOPosix/System.swift
@@ -86,15 +86,15 @@ let S_IFBLK = UInt32(SwiftGlibc.S_IFBLK)
 private let sysClose = close
 private let sysShutdown = shutdown
 private let sysBind = bind
-private let sysFcntl: (CInt, CInt, CInt) -> CInt = fcntl
+private let sysFcntl: @Sendable (CInt, CInt, CInt) -> CInt = fcntl
 private let sysSocket = socket
 private let sysSetsockopt = setsockopt
 private let sysGetsockopt = getsockopt
 private let sysListen = listen
 private let sysAccept = accept
 private let sysConnect = connect
-private let sysOpen: (UnsafePointer<CChar>, CInt) -> CInt = open
-private let sysOpenWithMode: (UnsafePointer<CChar>, CInt, mode_t) -> CInt = open
+private let sysOpen: @Sendable (UnsafePointer<CChar>, CInt) -> CInt = open
+private let sysOpenWithMode: @Sendable (UnsafePointer<CChar>, CInt, mode_t) -> CInt = open
 private let sysFtruncate = ftruncate
 private let sysWrite = write
 private let sysPwrite = pwrite
@@ -444,7 +444,7 @@ internal func syscallForbiddingEINVAL<T: FixedWidthInteger>(
 }
 
 @usableFromInline
-internal enum Posix {
+internal enum Posix: Sendable {
     #if canImport(Darwin)
     @usableFromInline
     static let UIO_MAXIOV: Int = 1024
@@ -1059,7 +1059,7 @@ extension Posix {
 
 #if canImport(Darwin)
 @usableFromInline
-internal enum KQueue {
+internal enum KQueue: Sendable {
 
     // TODO: Figure out how to specify a typealias to the kevent struct without run into trouble with the swift compiler
 
