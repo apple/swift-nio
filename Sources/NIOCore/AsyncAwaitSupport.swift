eb2a2d0d570f2901ef23e860f73d79cf4489e183
Cory Benfield <lukasa@apple.com>
2024-12-17 13:42:09 +0000

    Add simple Sendable conformance to AsyncSequenceFromIterator
    usableFromInline types need explicit Sendable conformances, so
    let's add one here. This type is a trivial wrapper, so let's
    express that

--- Sources/NIOCore/AsyncAwaitSupport.swift
+++ Sources/NIOCore/AsyncAwaitSupport.swift
@@ -424,6 +424,9 @@ struct AsyncSequenceFromIterator<AsyncIterator: AsyncIteratorProtocol>: AsyncSeq
 }
 
 @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
+extension AsyncSequenceFromIterator: Sendable where AsyncIterator: Sendable { }
+
+@available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
 extension EventLoop {
     @preconcurrency
     @inlinable
