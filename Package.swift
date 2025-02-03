a956c7407b11b527e4d2b48f51a761e917126d52
Cory Benfield <lukasa@apple.com>
2025-02-03 13:40:18 +0000

    Update NIOUDPEchoClient for strict concurrency
    Motivation:
    
    NIOUDPEchoClient should be strict concurrency clean.
    
    Modifications:
    
    Annotate one closure @Sendable to convince Swift it's
    ok.
    
    Result:
    
    Strict concurrency clean in NIOUDPEchoClient.

--- Package.swift
+++ Package.swift
@@ -382,7 +382,8 @@ let package = Package(
                 "NIOPosix",
                 "NIOCore",
             ],
-            exclude: ["README.md"]
+            exclude: ["README.md"],
+            swiftSettings: strictConcurrencySettings
         ),
         .executableTarget(
             name: "NIOAsyncAwaitDemo",
