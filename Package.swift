bdc82eb6393789b16c69987ab1db0762c7297081
Rick Newton-Rogers <rnro@apple.com>
2025-03-06 09:30:36 +0000

    Enable strict concurrency checking for NIOTestUtils
    Motivation:
    
    To ensure `NIOTestUtils` concurrency safety.
    
    Modifications:
    
    * Enable strict concurrency checking in the package manifest.
    * `NIOHTTP1TestServer` does pipeline configuration as synchronous
      operations.
    
    Result:
    
    `NIOTestUtils` does not warn of concurrency issues, builds will warn and CI will fail if regressions are introduced.

--- Package.swift
+++ Package.swift
@@ -233,7 +233,8 @@ let package = Package(
                 "NIOEmbedded",
                 "NIOHTTP1",
                 swiftAtomics,
-            ]
+            ],
+            swiftSettings: strictConcurrencySettings
         ),
         .target(
             name: "_NIOFileSystem",
