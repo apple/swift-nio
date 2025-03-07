72bc9eb9c9f69d8ca94c49b59d7917e55934a0f6
Rick Newton-Rogers <rnro@apple.com>
2025-03-07 13:14:13 +0000

    Only apply standard swift settings on valid targets

--- Package.swift
+++ Package.swift
@@ -598,11 +598,16 @@ if Context.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
 
 // ---    STANDARD CROSS-REPO SETTINGS DO NOT EDIT   --- //
 for target in package.targets {
-    if target.type != .plugin {
+    switch target.type {
+    case .regular, .test, .executable:
         var settings = target.swiftSettings ?? []
         // https://github.com/swiftlang/swift-evolution/blob/main/proposals/0444-member-import-visibility.md
         settings.append(.enableUpcomingFeature("MemberImportVisibility"))
         target.swiftSettings = settings
+    case .macro, .plugin, .system, .binary:
+        ()  // not applicable
+    @unknown default:
+        ()  // we don't know what to do here, do nothing
     }
 }
 // --- END: STANDARD CROSS-REPO SETTINGS DO NOT EDIT --- //
