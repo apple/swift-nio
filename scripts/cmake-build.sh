c2c8c30b87bfe506b802340a7e70262493eca34f
Rick Newton-Rogers <rnro@apple.com>
2025-03-13 11:03:23 +0000

    A re-usable CMake check workflow
    Motivation
    
    A few repositories have custom workflows for checking CMake, we would
    like to unify this for maintenance and adoption simplicity.
    
    Further to this at the moment they only check that the scripts are
    up-to-date, we'd also like to have them build using CMake.
    
    Modifications
    
    Add the re-usable workflow and supporting scripts
    
    Result:
    
    Repositories using CMake can switch to the centralised workflow.

---
+++ scripts/cmake-build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the SwiftNIO open source project
+##
+## Copyright (c) 2025 Apple Inc. and the SwiftNIO project authors
+## Licensed under Apache License v2.0
+##
+## See LICENSE.txt for license information
+## See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+##===----------------------------------------------------------------------===##
+
+set -uo pipefail
+
+log() { printf -- "** %s\n" "$*" >&2; }
+error() { printf -- "** ERROR: %s\n" "$*" >&2; }
+fatal() { error "$@"; exit 1; }
+
+version="${CMAKE_VERSION:="3.31.6"}"
+target_dir="${TARGET_DIRECTORY:=""}"
+
+if [ -z "$version" ]; then
+  fatal "CMake version must be specified."
+fi
+if [ -z "$target_dir" ]; then
+  fatal "Target directory must be specified."
+fi
+
+log "Installing tools for this script"
+apt-get update -y -q && apt-get install -y -q curl rsync
+
+log "Obtaining CMake (${CMAKE_VERSION})"
+curl -sL "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-x86_64.tar.gz" -o /tmp/cmake.tar.gz
+
+log "Installing CMake"
+mkdir -p /tmp/cmake
+tar xfvz /tmp/cmake.tar.gz --strip-components 1 -C /tmp/cmake
+rsync -q -a /tmp/cmake/* /usr/
+
+log "Installing Ninja"
+apt-get install -y -q ninja-build
+
+log "Building makefiles for target"
+build_directory="${TARGET_DIRECTORY}/build"
+mkdir -p "${build_directory}"
+cd "${build_directory}"
+cmake build -G Ninja -S ..
+
+log "Building target"
+ninja
