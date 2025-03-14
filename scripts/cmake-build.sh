f984b12a7ee3d0cc7e03053c5fb1c9a4143bcaf3
Rick Newton-Rogers <rnro@apple.com>
2025-03-14 11:20:33 +0000

    setup compilers & other dependencies

--- scripts/cmake-build.sh
+++ scripts/cmake-build.sh
@@ -19,33 +19,23 @@ log() { printf -- "** %s\n" "$*" >&2; }
 error() { printf -- "** ERROR: %s\n" "$*" >&2; }
 fatal() { error "$@"; exit 1; }
 
-version="${CMAKE_VERSION:="3.31.6"}"
 target_dir="${TARGET_DIRECTORY:=""}"
 
-if [ -z "$version" ]; then
-  fatal "CMake version must be specified."
-fi
 if [ -z "$target_dir" ]; then
   fatal "Target directory must be specified."
 fi
 
-log "Installing tools for this script"
-which curl || apt update -y -q && apt install -y -q curl
-
-log "Obtaining CMake (${CMAKE_VERSION})"
-curl -sL "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-x86_64.tar.gz" -o /tmp/cmake.tar.gz
-
-log "Installing CMake"
-tar xfvz /tmp/cmake.tar.gz --strip-components 1 -C /usr/
-
-log "Installing Ninja"
-apt install -y -q ninja-build
+CURL_BIN=${CURL_BIN:-$(which curl)} || fatal "CURL_BIN unset and no curl on PATH"
+TAR_BIN=${TAR_BIN:-$(which tar)} || fatal "TAR_BIN unset and no tar on PATH"
+CMAKE_BIN=${CMAKE_BIN:-$(which cmake)} || fatal "CMAKE_BIN unset and no cmake on PATH"
+NINJA_BIN=${NINJA_BIN:-$(which ninja)} || fatal "NINJA_BIN unset and no ninja on PATH"
+ASSEMBLY_COMPILER_BIN=${ASSEMBLY_COMPILER_BIN:-$(which clang)} || fatal "ASSEMBLY_COMPILER_BIN unset and no clang on PATH"
 
 log "Building Ninja build files for target"
-build_directory="${TARGET_DIRECTORY}/build"
-mkdir -p "${build_directory}"
-cd "${build_directory}" || fatal "Could not 'cd' to ${build_directory}"
-cmake build -G Ninja -S ..
+build_dir="${target_dir}/build"
+mkdir -p "$build_dir"
+cd "${build_dir}" || fatal "Could not 'cd' to ${build_dir}"
+ASM="$ASSEMBLY_COMPILER_BIN" "$CMAKE_BIN" build -G Ninja -S ..
 
 log "Building target"
-ninja
+"$NINJA_BIN"
