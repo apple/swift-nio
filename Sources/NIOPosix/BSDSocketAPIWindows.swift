aff71b37f50d8d062a2cf5cc968533f21523bd8a
Fabian Fett <fabianfett@apple.com>
2025-08-26 12:30:46 +0200

    [Windows] Use recv instead of read (#3357)

--- Sources/NIOPosix/BSDSocketAPIWindows.swift
+++ Sources/NIOPosix/BSDSocketAPIWindows.swift
@@ -720,7 +720,7 @@ extension NIOBSDSocket {
 extension IOVector {
     // An initializer thats maps `ByteBuffer`` derived pointers to WSABUF. This allows us to use the
     // same initializer, that we use for iovecs on Windows.
-    init(iov_base: UnsafeMutableRawPointer, iov_len: UInt32) {
+    init(iov_base: UnsafeMutableRawPointer!, iov_len: UInt32) {
         self = WSABUF(
             len: iov_len,
             buf: iov_base.assumingMemoryBound(to: Int8.self)
