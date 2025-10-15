42c21b89bc2a50f4feda45f4186941e26612b39c
MahdiBM <github@mahdibm.com>
2025-10-15 16:55:41 +0330

    Add lisense header

--- Sources/NIOCore/NIODecodedAsyncSequence.swift
+++ Sources/NIOCore/NIODecodedAsyncSequence.swift
@@ -1,4 +1,16 @@
-// MARK: NIODecodedAsyncSequence
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2025 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
 extension AsyncSequence where Element == ByteBuffer {
