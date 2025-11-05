0762843885459c081f61bc6aad99b636cdb614b8
Cory Benfield <lukasa@apple.com>
2025-11-05 12:26:39 -0500

    Enable per-message GSO on Linux
    Motivation:
    
    NIO has existing GSO support. This GSO support is applied at
    Channel scope, which makes it somewhat less than ideal, as it
    forces the datagram sizes to be known statically ahead of
    time. In real applications this is less common than
    being able to do this on a per-superbuffer
    context.
    
    Linux supports this already by using sendmsg/sendmmsg
    control data. We just need to wire this up in NIO.
    
    An extension to this is that we can also do per-message
    GRO in Linux. I'll tackle this in a later patch to keep
    the diffs small.
    
    Modifications:
    
    - Adding a new field on AddressedEnvelope Metadata.
    - Reject setting this field on non-Linux devices.
    - On Linux, use this to set the control data.
    - Added new tests that validate the behaviour is correct.
    
    Result:
    
    Enable per-message GSO.

--- Sources/NIOCore/AddressedEnvelope.swift
+++ Sources/NIOCore/AddressedEnvelope.swift
@@ -39,15 +39,32 @@ public struct AddressedEnvelope<DataType> {
         /// Details of any congestion state.
         public var ecnState: NIOExplicitCongestionNotificationState
         public var packetInfo: NIOPacketInfo?
+        /// The UDP segment size for Generic Segmentation Offload (GSO).
+        ///
+        /// When set, this enables per-message GSO, allowing the kernel to split this datagram
+        /// into multiple segments of the specified size. Only supported on Linux.
+        public var segmentSize: Int?
 
         public init(ecnState: NIOExplicitCongestionNotificationState) {
             self.ecnState = ecnState
             self.packetInfo = nil
+            self.segmentSize = nil
         }
 
         public init(ecnState: NIOExplicitCongestionNotificationState, packetInfo: NIOPacketInfo?) {
             self.ecnState = ecnState
             self.packetInfo = packetInfo
+            self.segmentSize = nil
+        }
+
+        public init(
+            ecnState: NIOExplicitCongestionNotificationState,
+            packetInfo: NIOPacketInfo?,
+            segmentSize: Int?
+        ) {
+            self.ecnState = ecnState
+            self.packetInfo = packetInfo
+            self.segmentSize = segmentSize
         }
     }
 }
