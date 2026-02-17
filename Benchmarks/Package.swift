2fdda6c78e6bc494eccc1c6707bd02a5979cfbe1
Fabian Fett <fabianfett@apple.com>
2026-02-17 11:30:44 +0100

    Fix benchmark succeeding – even though Benchmark build failed (#3502)
    ### Motivation:
    
    Currently if there is an error in the Benchmark targets, that prevents
    the Benchmark to build correctly, we still get a CI success.
    
    ### Modifications:
    
    - Check for the threshold changed error
    
    ### Result:
    
    - More reliable CI

--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../"),
-        .package(url: "https://github.com/ordo-one/package-benchmark.git", from: "1.22.0"),
+        .package(url: "https://github.com/ordo-one/package-benchmark.git", from: "1.29.11"),
     ],
     targets: [
         .executableTarget(
