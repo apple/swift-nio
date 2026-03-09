5b3cab2bb09f978cf4b1955a24ba6e369e3f4207
Cory Benfield <lukasa@apple.com>
2026-03-09 16:58:55 +0000

    Make MTELG's support for CPU pinning public (#3543)
    Motivation:
    
    NIO has had support for CPU pinning for 8 years, but nobody has ever
    used it. Ask me how I know?
    
    Modifications:
    
    - Make the CPU pinning constructor public.
    
    Result:
    
    Users can pin MTELG threads to specific CPU cores, _finally_.

--- Sources/NIOPosix/LinuxCPUSet.swift
+++ Sources/NIOPosix/LinuxCPUSet.swift
@@ -82,12 +82,14 @@ extension NIOThread {
 
 extension MultiThreadedEventLoopGroup {
 
-    /// Create a new `MultiThreadedEventLoopGroup` that create as many `NIOThread`s as `pinnedCPUIds`. Each `NIOThread` will be pinned to the CPU with the id.
+    /// Create a new `MultiThreadedEventLoopGroup` that create as many `NIOThread`s as `pinnedCPUIDs`. Each `NIOThread` will be pinned to the CPU with the id.
+    ///
+    /// Generally you should avoid using this API unless you have a concrete reason to need to pin threads to specific IDs.
     ///
     /// - arguments:
-    ///   - pinnedCPUIds: The CPU ids to apply to the `NIOThread`s.
-    convenience init(pinnedCPUIds: [Int]) {
-        let initializers: [ThreadInitializer] = pinnedCPUIds.map { id in
+    ///   - pinnedCPUIDs: The CPU ids to apply to the `NIOThread`s.
+    public convenience init(pinnedCPUIDs: [Int]) {
+        let initializers: [ThreadInitializer] = pinnedCPUIDs.map { id in
             // This will also take care of validation of the provided id.
             let set = LinuxCPUSet(id)
             return { t in
