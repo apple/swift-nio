bebdbec7a985788c84b4a504a348dc9dde6f6765
Arthur Liu <arthurliu1994@gmail.com>
2026-08-05 20:51:02 -0700

    Merge Windows FileInfo conditionals

--- Sources/_NIOFileSystem/FileInfo.swift
+++ Sources/_NIOFileSystem/FileInfo.swift
@@ -93,19 +93,8 @@ public struct FileInfo: Hashable, Sendable {
     public init(platformSpecificStatus: CInterop.Stat) {
         #if os(Windows)
         fatalError("FileInfo.allocatedSize is not implemented on Windows")
-        #endif
-        self._platformSpecificStatus = Stat(platformSpecificStatus)
-        #if os(Windows)
-        self.type = FileType(platformSpecificMode: platformSpecificStatus.nioMode)
-        self.permissions = FilePermissions(masking: platformSpecificStatus.nioMode)
-        self.size = platformSpecificStatus.nioSize
-        // Windows has no POSIX owner/group; report the defaults.
-        self.userID = UserID(rawValue: 0)
-        self.groupID = GroupID(rawValue: 0)
-        self.lastAccessTime = platformSpecificStatus.nioLastAccessTime
-        self.lastDataModificationTime = platformSpecificStatus.nioLastDataModificationTime
-        self.lastStatusChangeTime = platformSpecificStatus.nioLastStatusChangeTime
         #else
+        self._platformSpecificStatus = Stat(platformSpecificStatus)
         self.type = FileType(platformSpecificMode: CInterop.Mode(platformSpecificStatus.st_mode))
         self.permissions = FilePermissions(masking: CInterop.Mode(platformSpecificStatus.st_mode))
         self.size = Int64(platformSpecificStatus.st_size)
